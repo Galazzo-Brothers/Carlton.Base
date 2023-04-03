ba6a6ed79fb4ed3ed64407929fb821425dd57e52
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-04-03 09:00:18 -0400

    Added TestResults components

---
+++ src/Carlton.Base.TestBed/Components/TestResults/TestResultItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Carlton.Base.TestBedFramework;
+
+public record TestResultItem(string TestName, TestResultStatus TestResultStatus, double TestDuration);
+
+public enum TestResultStatus
+{
+    Passed,
+    Failed,
+    NotRun
+}
