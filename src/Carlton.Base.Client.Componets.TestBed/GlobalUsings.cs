8c2d197105e7ef8e09513b92132c0020e6d958eb
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-04-13 00:09:52 -0400

    naming convention cleanup

--- src/Carlton.Base.Client.Componets.TestBed/GlobalUsings.cs
+++ src/Carlton.Base.Client.Componets.TestBed/GlobalUsings.cs
@@ -4,7 +4,6 @@ global using System.Net.Http;
 global using System.Threading.Tasks;
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Carlton.Base.TestBedFramework;
 
