c3ef0006cebeff7e1866f246514dcf90000474d1
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-04-25 19:40:21 -0400

    Merged CarltonDataWrapper and CarltonDataComponent into single component

--- src/Carlton.Base.State/Contracts/ICarltonComponent.cs
+++ src/Carlton.Base.State/Contracts/ICarltonComponent.cs
@@ -3,5 +3,6 @@
 public interface ICarltonComponent<TViewModel>
 {
     TViewModel ViewModel { get; set; }
+    Func<Task<TViewModel>> GetViewModel { get; init; }
     EventCallback<ICommand> OnComponentEvent { get; init; }
 }
