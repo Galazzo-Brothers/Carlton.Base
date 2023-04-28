67f29772de0117e20b0e61f3198dcb14f6404297
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-04-28 17:15:10 -0400

    container

--- src/Carlton.Base.TestBed/Utils/WebAssemblyHostBuilderExtensions.cs
+++ src/Carlton.Base.TestBed/Utils/WebAssemblyHostBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Carlton.Base.TestBed;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Carlton.Base.TestBed;
 
 public static class WebAssemblyHostBuilderExtensions
 {
@@ -11,9 +13,10 @@ public static class WebAssemblyHostBuilderExtensions
         navTreeAct(NavMenuBuilder);
         var options = NavMenuBuilder.Build();
 
-        var state = new TestBedState(options);
-        builder.Services.AddSingleton(state);
+        var state = new TestBedStateProcessor(options);
+        builder.Services.AddSingleton<TestBedState>(state);
         builder.Services.AddSingleton<IStateStore<TestBedStateEvents>>(state);
+        builder.Services.AddSingleton<ICommandProcessor>(state);
         builder.Services.AddTransient<IViewModelStateFacade, TestBedViewModelStateFacade>();
         builder.Services.AddSingleton<ITrxParser, TrxParser>();
         builder.Services.AddMediatR(cfg =>
