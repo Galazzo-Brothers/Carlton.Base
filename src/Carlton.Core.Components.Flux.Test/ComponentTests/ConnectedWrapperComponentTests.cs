a02aa156e83044a6f1a74c9353a2a5e79eda25a9
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-08-26 02:11:54 -0400

    Added bUnit tests for connectedWrapper component

---
+++ src/Carlton.Core.Components.Flux.Test/ComponentTests/ConnectedWrapperComponentTests.cs
@@ -0,0 +1,161 @@
+﻿using Bunit;
+using Carlton.Core.Components.Flux.Contracts;
+using Carlton.Core.Components.Flux.Models;
+using Carlton.Core.Components.Flux.Test.Common;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Carlton.Core.Components.Flux.Test.ComponentTests;
+
+public class ConnectedWrapperComponentTests : TestContext
+{
+    private readonly Mock<IViewModelQueryDispatcher<TestState>> _vmDispatcher = new();
+    private readonly Mock<IMutationCommandDispatcher<TestState>> _mutationDispatcher = new();
+    private readonly Mock<IFluxStateObserver<TestState>> _observer = new();
+    private readonly Mock<ILogger<ConnectedWrapper<TestViewModel1, TestState>>> _logger = new();
+
+
+    public ConnectedWrapperComponentTests() 
+    {
+        //Arrange
+        Services.AddSingleton<IConnectedComponent<TestViewModel1>>(new DummyConnectedComponent());
+        Services.AddSingleton(_vmDispatcher.Object);
+        Services.AddSingleton(_mutationDispatcher.Object);
+        Services.AddSingleton(_observer.Object);
+        Services.AddSingleton(_logger.Object);
+
+        var vm = new TestViewModel1(7, "TestViewModel", "this is a crazy test");
+
+        _vmDispatcher.Setup(_ => _.Dispatch<TestViewModel1>(It.IsAny<ViewModelQuery>(), It.IsAny<CancellationToken>()))
+                                     .Returns(Task.FromResult(vm));
+    }
+
+    [Fact]
+    public void ConnectedWrapperRendersCorrectly()
+    {
+        //Arrange
+        var expectedMarkup = @"
+<div class=""vm-props"">
+  <span class=""id"">7</span>
+  <span class=""name"">TestViewModel</span>
+  <span class=""description"">this is a crazy test</span>
+  <button >Command Event Test</button>
+</div>";
+
+        // Act
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>();
+
+        // Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+
+    [Fact]
+    public void ConnectedWrapperInitializesCorrectly()
+    {
+        // Act
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>();
+
+        // Assert
+        _vmDispatcher.Verify(_ => _.Dispatch<TestViewModel1>(It.IsAny<ViewModelQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+    }
+
+
+    [Fact]
+    public void ConnectedWrapper_OnComponentEvent_CallsMutationDispatcher()
+    {
+        //Arrange
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>();
+        var buttonElement = cut.Find("button");
+
+        // Act
+        buttonElement.Click();
+
+        // Assert
+        _mutationDispatcher.Verify(_ => _.Dispatch(It.IsAny<TestCommand1>(), It.IsAny<CancellationToken>()), Times.Once());
+    }
+
+    [Fact]
+    public void ConnectedWrapper_ObservableStateEvents_InitializeCorrectly()
+    {
+        // Act
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>();
+
+        // Assert
+        Assert.Collection(cut.Instance.ObserveableStateEvents,
+          evt => Assert.Equal("TestEvent", evt),
+          evt => Assert.Equal("TestEvent2", evt),
+          evt => Assert.Equal("TestEvent3", evt)
+      );  
+    }
+
+    [Fact]
+    public void ConnectedWrapper_OnStateChangeTestEvent_CallsViewModelDispatcher()
+    {
+        // Arrange
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>();
+        var times = Times.Exactly(2); //Once for the component init and once again for the state change
+
+        //Act
+        _observer.Raise(_ => _.StateChanged += null, new object[] { "TestEvent" });
+
+        // Assert
+        _vmDispatcher.Verify(_ => _.Dispatch<TestViewModel1>(It.IsAny<ViewModelQuery>(), It.IsAny<CancellationToken>()), times);
+    }
+
+    [Fact]
+    public void ConnectedWrapper_OnStateChangeNonListeningEvent_DoesNotCallViewModelDispatcher()
+    {
+        // Arrange
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>();
+        var times = Times.Once(); //One and only time for the component init
+
+        //Act
+        _observer.Raise(_ => _.StateChanged += null, new object[] { "TestState4" });
+
+        // Assert
+        _vmDispatcher.Verify(_ => _.Dispatch<TestViewModel1>(It.IsAny<ViewModelQuery>(), It.IsAny<CancellationToken>()), times);
+    }
+
+    [Fact]
+    public void ConnectedWrapperDisposesCorrectly()
+    {
+        //Arrange
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>();
+        var times = Times.Once(); //One and only time for the component init, event handler removed correctly during dispose
+        DisposeComponents();
+
+        //Act
+        _observer.Raise(_ => _.StateChanged += null, new object[] { "TestEvent" });
+
+        // Assert
+        _vmDispatcher.Verify(_ => _.Dispatch<TestViewModel1>(It.IsAny<ViewModelQuery>(), It.IsAny<CancellationToken>()), times);
+    }
+
+    [Fact]
+    public void ConnectedWrapper_LoadingContent()
+    {
+        //Arrange
+        var spinnerMarkup = "<span class='spinner'>This is a spinner.</span>";
+        var renderFragment = new RenderFragment(builder =>
+        {
+            builder.AddMarkupContent(0, spinnerMarkup);
+        });
+
+        var propInfo = typeof(ConnectedWrapper<TestViewModel1, TestState>)
+            .GetProperty("IsLoading");
+
+        var cut = RenderComponent<ConnectedWrapper<TestViewModel1, TestState>>(parameters => parameters
+            .Add(p => p.SpinnerContent, renderFragment));
+
+        //Act
+        propInfo.SetValue(cut.Instance, true);
+        cut.Render();
+
+        //Assert
+        cut.MarkupMatches(spinnerMarkup);
+    }
+}
+
+
