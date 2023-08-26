c57919d2512bf8d347bf3cf5c144cf5bda1a02bc
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-08-26 13:13:23 -0400

    refactor flux state registrations

--- src/Lab/Carlton.Core.Components.Lab/Utils/WebAssemblyHostBuilderExtensions.cs
+++ src/Lab/Carlton.Core.Components.Lab/Utils/WebAssemblyHostBuilderExtensions.cs
@@ -13,19 +13,12 @@ public static class WebAssemblyHostBuilderExtensions
         navTreeAct(NavMenuBuilder);
         var options = NavMenuBuilder.Build();
 
-        /*LabState*/
-        var state = new LabState(options, testResults);
-        builder.Services.AddSingleton(state);
-        builder.Services.AddSingleton<FluxState<LabState>>();
-        builder.Services.AddSingleton<IFluxStateObserver<LabState>>(_ => _.GetService<FluxState<LabState>>());
-        builder.Services.AddSingleton<IMutableFluxState<LabState>>(_ => _.GetService<FluxState<LabState>>());
-        builder.Services.AddSingleton<MutationResolver<LabState>>();
-
         /*Mapster Extensions*/
         MapsterConfig.RegisterMapsterConfiguration();
 
         /*Flux Registers*/
-        builder.Services.AddCarltonFlux<LabState>();
+        var state = new LabState(options, testResults);
+        builder.Services.AddCarltonFlux(state);
     }
 }
 
