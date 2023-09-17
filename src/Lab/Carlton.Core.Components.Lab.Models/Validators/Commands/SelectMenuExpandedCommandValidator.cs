16ebf03004c393383696705dd66ae1e8f9113df7
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-09-17 11:19:05 -0400

    fixed NavMenu component and validators

---
+++ src/Lab/Carlton.Core.Components.Lab.Models/Validators/Commands/SelectMenuExpandedCommandValidator.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Carlton.Core.Components.Lab.Models.Validators.Commands;
+
+public class SelectMenuExpandedCommandValidator : AbstractValidator<SelectMenuExpandedCommand>
+{
+    public SelectMenuExpandedCommandValidator()
+    {
+        RuleFor(command => command.SelectedComponentIndex).GreaterThanOrEqualTo(0);
+    }
+}
