440ff2b336c752249360f3bcc1e3d2817e10a24a
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-11-17 14:38:53 -0500

    Added custom converters for Type and EventId

---
+++ src/Utilities/Carlton.Core.Utilities/JsonConverters/JsonEventIdConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Carlton.Core.Utilities.JsonConverters;
+
+public class JsonEventIdConverter : JsonConverter<EventId>
+{
+    public override EventId Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        using JsonDocument doc = JsonDocument.ParseValue(ref reader);
+        // Implement deserialization logic here
+        var root = doc.RootElement;
+        int id = root.GetProperty("Id").GetInt32();
+        string name = root.GetProperty("Name").GetString();
+        return new EventId(id, name);
+    }
+
+    public override void Write(Utf8JsonWriter writer, EventId value, JsonSerializerOptions options)
+    {
+        // Implement serialization logic here
+        writer.WriteStartObject();
+        writer.WriteNumber("Id", value.Id);
+        writer.WriteString("Name", value.Name);
+        writer.WriteEndObject();
+    }
+}
---
+++ src/Utilities/Carlton.Core.Utilities/JsonConverters/JsonTypeConverter.cs
@@ -0,0 +1,19 @@
+﻿namespace Carlton.Core.Utilities.JsonConverters;
+
+public class JsonTypeConverter : JsonConverter<Type>
+{
+    public override Type Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        string typeName = reader.GetString();
+        return Type.GetType(typeName);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Type value, JsonSerializerOptions options)
+    {
+        if (value is not null)
+        {
+            writer.WriteStringValue(value.AssemblyQualifiedName);
+        }
+    }
+}
+
