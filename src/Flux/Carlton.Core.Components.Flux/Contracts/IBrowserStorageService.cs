76eb0de202848327972a4c94da17d5bc11a301dc
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-12-13 14:43:27 -0500

    browser storage service now uses eventes to notify when logs have been cleared

--- src/Flux/Carlton.Core.Components.Flux/Contracts/IBrowserStorageService.cs
+++ src/Flux/Carlton.Core.Components.Flux/Contracts/IBrowserStorageService.cs
@@ -4,6 +4,7 @@ namespace Carlton.Core.Components.Flux.Contracts;
 
 public interface IBrowserStorageService
 {
+    public event Action LogsCleared;
     public Task CommitLogs();
     public Task<IEnumerable<LogMessage>> GetLogs(DateTime dateTime);
     public Task ClearLogs();
