b00ce3f18c03d2fb7b066d9b3a73663179a7f3d4
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-12-13 19:10:01 -0500

    styling tweaks

--- src/Carlton.Core.Components.Flux.Admin/Extensions/ContainerExtensions.cs
+++ src/Carlton.Core.Components.Flux.Admin/Extensions/ContainerExtensions.cs
@@ -1,17 +1,12 @@
-﻿using BlazorDB;
-using Carlton.Core.Utilities.Logging;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace Carlton.Core.Components.Flux.Admin;
 
 public static class ContainerExtensions
 {
-    public static void AddCarltonFluxAdmin<TState>(this IServiceCollection services, IConfigurationRoot configuration)
+    public static void AddCarltonFluxAdmin<TState>(this WebAssemblyHostBuilder builder, TState state)
     {
-
-     
     }
 }
 
