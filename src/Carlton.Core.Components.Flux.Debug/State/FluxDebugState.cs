992c37514db705b10197b01e917ade9317ef4d74
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-12-20 18:31:14 -0500

    clean around the base connected components

--- src/Carlton.Core.Components.Flux.Debug/State/FluxDebugState.cs
+++ src/Carlton.Core.Components.Flux.Debug/State/FluxDebugState.cs
@@ -2,14 +2,14 @@ using Carlton.Core.Utilities.Logging;
 
 namespace Carlton.Core.Components.Flux.Debug.State;
 
-public class FluxDebugState<TState>
+public class FluxDebugState
 {
-    public FluxDebugState(TState state, IEnumerable<LogMessage> logMessages)
+    public FluxDebugState(object state, IEnumerable<LogMessage> logMessages)
     {
         State = state;
         LogMessages = logMessages;
     }
 
     public IEnumerable<LogMessage> LogMessages { get; private set; }
-    public TState State { get; private set; }
+    public object State { get; private set; }
 }
