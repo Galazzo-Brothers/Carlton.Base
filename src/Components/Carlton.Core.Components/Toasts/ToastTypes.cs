c9131dc47b583678d3a4df0074033a954803a2a9
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-12-28 18:56:03 -0500

    clean up around toast notifications

---
+++ src/Components/Carlton.Core.Components/Toasts/ToastTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Carlton.Core.Components.Toasts;
+
+public enum ToastTypes
+{
+    Success = 1,
+    Info = 2,
+    Warning = 3,
+    Error = 4,
+}
---
+++ src/Components/Carlton.Core.Components/Toasts/ToastViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Carlton.Core.Components.Toasts;
+
+public record ToastViewModel
+{
+    public string Title { get; init; }
+    public string Message { get; init; }
+    public ToastTypes ToastType { get; init; }
+    public bool FadeOutEnabled { get; init; }
+    public int ToastIndex { get; init; }
+    public Action OnDismiss { get; init; }
+}
