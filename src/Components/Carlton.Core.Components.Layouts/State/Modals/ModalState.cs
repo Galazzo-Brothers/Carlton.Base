34674a089d2b9f6c0cf88980344927ea85001825
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-12-29 11:08:53 -0500

    clean up around ModalState

--- src/Components/Carlton.Core.Components.Layouts/State/Modals/ModalState.cs
+++ src/Components/Carlton.Core.Components.Layouts/State/Modals/ModalState.cs
@@ -7,6 +7,7 @@ public class ModalState : IModalState
     public event EventHandler<ModalRaisedEventArgs> ModalRaised;
 
     public ModalViewModel ModalModel { get; private set; }
+    public Type ModalType { get; private set; }
 
     public ModalState()
     {
@@ -16,7 +17,11 @@ public class ModalState : IModalState
     public void RaiseModal<TModal>(ModalViewModel model)
         where TModal : Modal
     {
+        if (!typeof(TModal).IsAssignableTo(typeof(Modal)))
+            throw new ArgumentException($"{typeof(TModal)} is not a valid Modal type.");
+
         ModalModel = model;
+        ModalType = typeof(TModal);
         ModalRaised?.Invoke(this, new ModalRaisedEventArgs(typeof(TModal)));
     }
 }
