0121381bf01698ebe103616399dcb6e77be96334
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2023-12-29 19:55:42 -0500

    moved mediaQueryWrapper to layout package. Created a ViewPortState service for interacting with the browser viewport information. MediaQueryWrapper now respects that and does not render both mobile and desktop

---
+++ src/Components/Carlton.Core.Components.Layouts/State/Viewport/ViewportChangedEventArgs.cs
@@ -0,0 +1,6 @@
+﻿namespace Carlton.Core.Components.Layouts.State.Viewport;
+
+public class ViewportChangedEventArgs(ViewportModel viewportModel) : EventArgs
+{
+    public ViewportModel ViewportModel { get; set; } = viewportModel;
+}
