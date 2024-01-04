48cd2a9d6e0cd9103b916de751abf8678a890726
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-01-04 09:38:03 -0500

    styling tweaks to the resizable panel

--- src/Components/Carlton.Core.Components/ResizablePanel/ResizablePanel.razor.js
+++ src/Components/Carlton.Core.Components/ResizablePanel/ResizablePanel.razor.js
@@ -1,6 +1,7 @@
 ﻿export function initResizablePanel() {
     const bar = document.querySelector('.splitter-horizontal');
     const top = document.querySelector('.panel-top');
+    const topHeightOffset = 100;
     let mouse_is_down = false;
 
     bar.addEventListener('mousedown', (e) => {
@@ -11,7 +12,7 @@
         if (!mouse_is_down) return;
 
         top.style.cursor = "row-resize";
-        top.style.height = `${e.clientY}px`;
+        top.style.height = `${e.clientY - topHeightOffset}px`;
     });
 
     document.addEventListener('mouseup', () => {
