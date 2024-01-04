9680143cac7fa36bd1c73498f0dfe43040c41e8e
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-01-04 14:49:31 -0500

    MemoryLogger makes use LogMessage object

--- src/Utilities/Carlton.Core.Utilities/Logging/LogMessage.cs
+++ src/Utilities/Carlton.Core.Utilities/Logging/LogMessage.cs
@@ -3,24 +3,10 @@
 public record LogMessage
 {
     public LogLevel LogLevel { get; init; }
-    public int EventId { get; init; }
-    public string EventName { get; init; }
+    public EventId EventId { get; init; }
     public string Message { get; init; }
-    public LoggedException LoggedException { get; init; }
+    public Exception Exception { get; init; }
     public DateTime Timestamp { get; init; }
     public string Scopes { get; init; }
 }
 
-public record LoggedException
-{
-    public string ExceptionType { get; init; }
-    public string Message { get; init; }    
-    public string StackTrace { get; init; }
-
-    public LoggedException()
-    {
-    }
-
-    public LoggedException(Exception exception)
-        => (ExceptionType, Message, StackTrace) = (exception?.GetType()?.ToString(), exception?.Message, exception?.StackTrace);  
-}
