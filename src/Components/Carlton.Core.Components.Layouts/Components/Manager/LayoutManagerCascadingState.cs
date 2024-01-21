104b3864b0d5232d942e771cf0f788e48b21b26e
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-01-20 22:22:13 -0500

    added a LayoutManager and converted layouts to use new Section feature

---
+++ src/Components/Carlton.Core.Components.Layouts/Components/Manager/LayoutManagerCascadingState.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Carlton.Core.Components.Layouts;
+
+public record class LayoutManagerCascadingState
+{
+    public required bool IsFullScreen { get; init; }
+    public IDictionary<string, object> LayoutSettings { get; init; }
+}
--- src/Components/Carlton.Core.Components.Layouts/Extensions/Extensions.cs
+++ src/Components/Carlton.Core.Components.Layouts/Extensions/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using Carlton.Core.Components.Layouts.State.FullScreen;
+using Carlton.Core.Components.Layouts.State.LayoutSettings;
 using Carlton.Core.Components.Layouts.State.Modals;
 using Carlton.Core.Components.Layouts.State.Theme;
 using Carlton.Core.Components.Layouts.State.Toasts;
@@ -9,12 +10,16 @@ namespace Carlton.Core.Components.Layouts.Extensions;
 
 public static class Extensions
 {
-    public static void AddCarltonLayout(this IServiceCollection services)
+    public static void AddCarltonLayout(this IServiceCollection services, Action<LayoutSettingsBuilder> act)
     {
+        var builder = new LayoutSettingsBuilder();
+        act(builder);
+
         services.AddSingleton<IFullScreenState, FullScreenState>();
         services.AddSingleton<IThemeState, ThemeState>();
         services.AddSingleton<IModalState, ModalState>();
         services.AddSingleton<IToastState, ToastState>();
         services.AddSingleton<IViewportState, ViewportState>();
+        services.AddSingleton<ILayoutSettings>(builder.Build());
     }
 }
