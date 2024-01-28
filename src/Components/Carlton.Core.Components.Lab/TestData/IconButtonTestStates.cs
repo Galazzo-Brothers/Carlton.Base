f8b11e98d23f87ed1747c9552b4fe3659a1d075b
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-01-28 12:59:22 -0500

    added new button components

---
+++ src/Components/Carlton.Core.Components.Lab/TestData/IconButtonTestStates.cs
@@ -0,0 +1,12 @@
+﻿namespace Carlton.Core.Components.Library.Lab.TestData;
+
+internal static class IconButtonTestStates
+{
+    public static object DefaultState
+    {
+        get => new
+        {
+            Icon = "delete"
+        };
+    }
+}
