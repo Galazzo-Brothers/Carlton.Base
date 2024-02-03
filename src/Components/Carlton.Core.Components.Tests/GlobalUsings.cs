3e5621f2c4662ce5abd34e0f7c7dae08b56f1aa4
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-03 15:02:04 -0500

    more lcean up

--- src/Components/Carlton.Core.Components.Tests/GlobalUsings.cs
+++ src/Components/Carlton.Core.Components.Tests/GlobalUsings.cs
@@ -1,6 +1,7 @@
-global using System.Collections.ObjectModel;
-global using System.ComponentModel;
 global using System.Web;
 global using Microsoft.AspNetCore.Components;
 global using Xunit;
 global using Bunit;
+global using Shouldly;
+global using AutoFixture.Xunit2;
+global using Carlton.Core.Utilities.Random;
