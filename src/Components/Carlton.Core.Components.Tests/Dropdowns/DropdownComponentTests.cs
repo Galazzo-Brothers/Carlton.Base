bea61cf52842bbc2491ed4a0fae47c5cb07fca1c
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-04 15:57:40 -0500

    dropdowns now have genric values

--- src/Components/Carlton.Core.Components.Tests/Dropdowns/DropdownComponentTests.cs
+++ src/Components/Carlton.Core.Components.Tests/Dropdowns/DropdownComponentTests.cs
@@ -1,7 +1,7 @@
 ﻿using Carlton.Core.Components.Dropdowns;
 namespace Carlton.Core.Components.Tests;
 
-[Trait("Component", nameof(Dropdown))]
+[Trait("Component", nameof(Dropdown<int>))]
 public class DropdownComponentTests : TestContext
 {
     [Theory(DisplayName = "Markup Test")]
@@ -10,20 +10,21 @@ public class DropdownComponentTests : TestContext
     public void Dropdown_Markup_RendersCorrectly(
         bool expectedIsDisabled,
         IReadOnlyDictionary<string, int> expectedItems,
-        string expectedLabelText)
+        string expectedLabelText,
+        bool expectedIsPristineEnabled)
     {
         //Arrange
-        var index = RandomUtilities.GetRandomIndex(expectedItems.Count);
-        var selectedItem = expectedItems.ElementAt(index);
-        var selectedValue = selectedItem.Value;
-        var expectedMarkup = BuildExpectedMarkup(expectedLabelText, selectedItem.Key, expectedIsDisabled, expectedItems);
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
+        var (selectedKey, selectedValue) = expectedItems.ElementAt(expectedIndex);
+        var expectedMarkup = BuildExpectedMarkup(expectedLabelText, selectedKey, expectedIsDisabled, expectedIsPristineEnabled, expectedItems);
 
         //Act
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
             .Add(p => p.Options, expectedItems)
             .Add(p => p.Label, expectedLabelText)
             .Add(p => p.IsDisabled, expectedIsDisabled)
-            .Add(p => p.SelectedValue, selectedValue));
+            .Add(p => p.SelectedIndex, expectedIndex)
+            .Add(p => p.IsPristineEnabled, expectedIsPristineEnabled));
 
         //Assert
         cut.MarkupMatches(expectedMarkup);
@@ -33,15 +34,18 @@ public class DropdownComponentTests : TestContext
     public void Dropdown_LabelParameter_RendersCorrectly(
         IReadOnlyDictionary<string, int> expectedItems,
         string expectedLabelText,
-        bool expectedIsDisabled,
-        int expectedSelectedValue)
+        bool expectedIsDisabled)
     {
+        //Arrange
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
+
         //Act
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
             .Add(p => p.Options, expectedItems)
             .Add(p => p.Label, expectedLabelText)
             .Add(p => p.IsDisabled, expectedIsDisabled)
-            .Add(p => p.SelectedValue, expectedSelectedValue));
+            .Add(p => p.SelectedIndex, expectedIndex));
 
         var labelContent = cut.Find(".label").TextContent;
 
@@ -55,15 +59,17 @@ public class DropdownComponentTests : TestContext
     public void Dropdown_DisabledParameter_RendersCorrectly(
         bool expectedIsDisabled,
         IReadOnlyDictionary<string, int> expectedItems,
-        string expectedLabelText,
-        int expectedSelectedValue)
+        string expectedLabelText)
     {
+        //Arrange
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
+
         //Act
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
             .Add(p => p.Options, expectedItems)
             .Add(p => p.Label, expectedLabelText)
             .Add(p => p.IsDisabled, expectedIsDisabled)
-            .Add(p => p.SelectedValue, expectedSelectedValue));
+            .Add(p => p.SelectedIndex, expectedIndex));
 
         var optionsElement = cut.Find(".options");
         var containsDisabledAttribute = optionsElement.Attributes.Select(_ => _.Name).Contains("disabled");
@@ -75,19 +81,19 @@ public class DropdownComponentTests : TestContext
     [Theory(DisplayName = "Options Parameter Tests"), AutoData]
     public void Dropdown_OptionsParameterCount_RendersCorrectly(
         IReadOnlyDictionary<string, int> expectedItems,
-        string expectedLabelText,
-        int expectedSelectedValue)
+        string expectedLabelText)
     {
         //Arrange
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
         var expectedCount = expectedItems.Count;
         var expectedOptionNames = expectedItems.Keys;
 
         //Act
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
             .Add(p => p.Options, expectedItems)
             .Add(p => p.Label, expectedLabelText)
             .Add(p => p.IsDisabled, false)
-            .Add(p => p.SelectedValue, expectedSelectedValue));
+            .Add(p => p.SelectedIndex, expectedIndex));
 
         var optionsElements = cut.FindAll(".option");
         var actualCount = optionsElements.Count;
@@ -99,15 +105,17 @@ public class DropdownComponentTests : TestContext
     [Theory(DisplayName = "Selected Options Parameter Render Test"), AutoData]
     public void Dropdown_OptionsParameter_RendersCorrectly(
         IReadOnlyDictionary<string, int> expectedItems,
-        string expectedLabelText,
-        int expectedSelectedValue)
+        string expectedLabelText)
     {
+        //Arrange
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
+
         //Act
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
             .Add(p => p.Options, expectedItems)
             .Add(p => p.Label, expectedLabelText)
             .Add(p => p.IsDisabled, false)
-            .Add(p => p.SelectedValue, expectedSelectedValue));
+            .Add(p => p.SelectedIndex, expectedIndex));
 
         var optionsElements = cut.FindAll(".option");
 
@@ -123,15 +131,16 @@ public class DropdownComponentTests : TestContext
         string expectedLabelText)
     {
         //Arrange
-        var index = RandomUtilities.GetRandomIndex(expectedItems.Count);
-        var (selectedKey, selectedValue) = expectedItems.ElementAt(index);
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
+        var (selectedKey, selectedValue) = expectedItems.ElementAt(expectedIndex);
 
         //Act
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
             .Add(p => p.Options, expectedItems)
             .Add(p => p.Label, expectedLabelText)
             .Add(p => p.IsDisabled, false)
-            .Add(p => p.SelectedValue, selectedValue));
+            .Add(p => p.SelectedIndex, expectedIndex)
+            .Add(p => p.IsPristineEnabled, false));
 
         var inputElement = cut.Find("input");
         var valueDisplay = inputElement?.Attributes["value"]?.TextContent;
@@ -145,12 +154,16 @@ public class DropdownComponentTests : TestContext
         IReadOnlyDictionary<string, int> expectedItems,
         string expectedLabelText)
     {
+        //Arrange
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
+
         //Act
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
               .Add(p => p.Options, expectedItems)
               .Add(p => p.Label, expectedLabelText)
               .Add(p => p.IsDisabled, false)
-              .Add(p => p.SelectedValue, -1));
+              .Add(p => p.SelectedIndex, expectedIndex)
+              .Add(p => p.IsPristineEnabled, true));
 
         var inputElement = cut.Find("input");
         var containsValueAttribute = inputElement.Attributes.Any(_ => _.Name == "value");
@@ -162,22 +175,20 @@ public class DropdownComponentTests : TestContext
     [Theory(DisplayName = "ValueChangedCallback Parameter Test"), AutoData]
     public void Dropdown_ValueChangedCallbackParameter_FiresCallback(
         IReadOnlyDictionary<string, int> expectedItems,
-        string expectedLabelText,
-        int expectedSelectedValue)
+        string expectedLabelText)
     {
         //Arrange
-        var index = RandomUtilities.GetRandomIndex(expectedItems.Count);
+        var expectedIndex = RandomUtilities.GetRandomIndex(expectedItems.Count);
         var eventFired = false;
         var eventKey = string.Empty;
         var eventValue = -1;
+        var (expectedKey, expectedValue) = expectedItems.ElementAt(expectedIndex);
 
-        var (expectedKey, expectedValue) = expectedItems.ElementAt(index);
-
-        var cut = RenderComponent<Dropdown>(parameters => parameters
+        var cut = RenderComponent<Dropdown<int>>(parameters => parameters
            .Add(p => p.Options, expectedItems)
            .Add(p => p.Label, expectedLabelText)
            .Add(p => p.IsDisabled, false)
-           .Add(p => p.SelectedValue, expectedSelectedValue)
+           .Add(p => p.SelectedIndex, expectedIndex)
            .Add(p => p.ValueChangedCallback, (args) =>
                 {
                     eventFired = true;
@@ -186,7 +197,7 @@ public class DropdownComponentTests : TestContext
                 }));
 
         //Act
-        cut.FindAll(".option")[index].Click();
+        cut.FindAll(".option")[expectedIndex].Click();
 
         //Assert
         eventFired.ShouldBeTrue();
@@ -195,11 +206,11 @@ public class DropdownComponentTests : TestContext
     }
 
 
-    private static string BuildExpectedMarkup(string labelText, string selectedItem, bool isDisabled, IReadOnlyDictionary<string, int> options)
+    private static string BuildExpectedMarkup(string labelText, string selectedItem, bool isDisabled, bool isPristineEnabled, IReadOnlyDictionary<string, int> options)
     {
         return
 @$"
-<div class=""dropdown""><input readonly placeholder="" "" value=""{selectedItem}"" />
+<div class=""dropdown""><input readonly placeholder="" "" value=""{(isPristineEnabled ? null : selectedItem)}"" />
     <div class=""label"">{labelText}</div>
     <div {(isDisabled ? "disabled = \"\"" : string.Empty)} class=""options"">
         {BuildExpectedOptionsMarkup(isDisabled, options)}
