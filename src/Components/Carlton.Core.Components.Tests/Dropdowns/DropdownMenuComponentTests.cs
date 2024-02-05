5949f15c9fac168d206ff2ab1483021fd506d74c
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-05 00:07:55 -0500

    tweaks around Dropdown menu components added missing unit tests

---
+++ src/Components/Carlton.Core.Components.Tests/Dropdowns/DropdownMenuComponentTests.cs
@@ -0,0 +1,155 @@
+﻿using Carlton.Core.Components.Dropdowns;
+namespace Carlton.Core.Components.Tests;
+
+[Trait("Component", nameof(DropdownMenu<int>))]
+public class DropdownMenuComponentTests : TestContext
+{
+    [Theory(DisplayName = "Markup Test"), AutoData]
+    public void DropdownMenu_Markup_RendersCorrectly(
+      IEnumerable<DropdownMenuItem<int>> expectedMenuItems,
+      string expectedMenuTemplate,
+      string expectedHeaderTemplate)
+    {
+        //Arrange
+        var expectedMarkup = BuildExpectedMarkup(expectedMenuItems, expectedMenuTemplate, expectedHeaderTemplate);
+
+        //Act
+        var cut = RenderComponent<DropdownMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.MenuTemplate, expectedMenuTemplate)
+            .Add(p => p.HeaderTemplate, expectedHeaderTemplate));
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+
+    [Theory(DisplayName = "MenuTemplate Parameter Test"), AutoData]
+    public void DropdownMenu_MenuTemplateParameter_RendersCorrectly(
+       IEnumerable<DropdownMenuItem<int>> expectedMenuItems,
+       string expectedMenuTemplate,
+       string expectedHeaderTemplate)
+    {
+        //Act
+        var cut = RenderComponent<DropdownMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.MenuTemplate, expectedMenuTemplate)
+            .Add(p => p.HeaderTemplate, expectedHeaderTemplate));
+
+        var actualMenuContent = cut.Find(".menu").TextContent;
+
+        //Assert
+        actualMenuContent.ShouldBe(expectedMenuTemplate);
+    }
+
+    [Theory(DisplayName = "HeaderTemplate Parameter Test"), AutoData]
+    public void DropdownMenu_HeaderTemplateParemter_RendersCorrectly(
+       IEnumerable<DropdownMenuItem<int>> expectedMenuItems,
+       string expectedMenuTemplate,
+       string expectedHeaderTemplate)
+    {
+        //Act
+        var cut = RenderComponent<DropdownMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.MenuTemplate, expectedMenuTemplate)
+            .Add(p => p.HeaderTemplate, expectedHeaderTemplate));
+
+        var actualHeaderContent = cut.Find(".header").TextContent;
+
+        //Assert
+        actualHeaderContent.ShouldBe(expectedHeaderTemplate);
+    }
+
+    [Theory(DisplayName = "MenuItems Parameter Test"), AutoData]
+    public void DropdownMenu_MenuItemsParameter_RendersCorrectly(
+        IEnumerable<DropdownMenuItem<int>> expectedMenuItems,
+        string expectedMenuTemplate,
+        string expectedHeaderTemplate)
+    {
+        //Act
+        var cut = RenderComponent<DropdownMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.MenuTemplate, expectedMenuTemplate)
+            .Add(p => p.HeaderTemplate, expectedHeaderTemplate));
+
+        var actualItemContent = cut.FindAll("li a").Select(_ => _.TextContent);
+        var expectedHeaderContent = expectedMenuItems.Select(_ => _.MenuItemName);
+
+        //Assert
+        actualItemContent.Count().ShouldBe(expectedMenuItems.Count());
+        actualItemContent.ShouldBe(expectedHeaderContent);
+    }
+
+    [Theory(DisplayName = "MenuItems Parameter Callback Test"), AutoData]
+    public void DropdownMenu_MenuItemsParameterCallback_RendersCorrectly(
+        IEnumerable<DropdownMenuItem<int>> expectedMenuItems,
+        string expectedMenuTemplate,
+        string expectedHeaderTemplate)
+    {
+        //Arrange
+        var selectedIndex = RandomUtilities.GetRandomIndex(expectedMenuItems.Count());
+        var callbackFired = false;
+        var callbackIndex = -1;
+        var expectedItemsWithCallback = expectedMenuItems.Select((item, i) => 
+            item with
+            {
+                MenuItemSelected = () =>
+                {
+                    callbackFired = true;
+                    callbackIndex = i;
+                }
+            });
+
+        var cut = RenderComponent<DropdownMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedItemsWithCallback)
+            .Add(p => p.MenuTemplate, expectedMenuTemplate)
+            .Add(p => p.HeaderTemplate, expectedHeaderTemplate));
+
+        //Act
+        cut.FindAll("a").ElementAt(selectedIndex).Click();
+
+        //Assert
+        callbackFired.ShouldBeTrue();
+        callbackIndex.ShouldBe(selectedIndex);
+    }
+
+    [Theory(DisplayName = "MenuItems Expanded Options Click Test"), AutoData]
+    public void DropdownMenu_ExpandOptionsInteraction_RendersCorrectly(
+       IEnumerable<DropdownMenuItem<int>> expectedMenuItems,
+       string expectedMenuTemplate,
+       string expectedHeaderTemplate)
+    {
+        //Arrange
+        var selectedIndex = RandomUtilities.GetRandomIndex(expectedMenuItems.Count());
+        var cut = RenderComponent<DropdownMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.MenuTemplate, expectedMenuTemplate)
+            .Add(p => p.HeaderTemplate, expectedHeaderTemplate));
+
+        //Act
+        cut.Find(".dropdown-menu").Click();
+        var itemsElement = cut.Find(".dropdown-items");
+
+        //Assert
+        itemsElement.ClassList.ShouldContain("active");
+    }
+
+    private static string BuildExpectedMarkup(IEnumerable<DropdownMenuItem<int>> expectedMenuItems, string expectedMenuTemplate, string expectedHeaderTemplate)
+    {
+        var itemElements = string.Join(Environment.NewLine, expectedMenuItems.Select(item => $@"
+        <li>
+            <span class=""mdi mdi-{item.MenuIcon} accent-color-{item.AccentColorIndex}"" ></span>
+            <a href=""#"">{item.MenuItemName}</a>
+        </li>"));
+
+        var expectedMarkup = $@"
+        <div class=""dropdown-menu"">
+            <div class=""menu"">{expectedMenuTemplate}</div>
+            <div class=""dropdown-items"">
+            <div class=""header"">{expectedHeaderTemplate}</div>
+            <ul>{itemElements}</ul>
+          </div>
+        </div>";
+
+        return expectedMarkup;
+    }
+}
---
+++ src/Components/Carlton.Core.Components.Tests/Dropdowns/KebabMenuComponentTests.cs
@@ -0,0 +1,129 @@
+﻿using Carlton.Core.Components.Dropdowns;
+namespace Carlton.Core.Components.Tests;
+
+[Trait("Component", nameof(KebabMenu<int>))]
+public class KebabMenuComponentTests : TestContext
+{
+    [Theory(DisplayName = "Markup Test")]
+    [InlineAutoData(true)]
+    [InlineAutoData(false)]
+    public void KebabMenu_Markup_RendersCorrectly(
+        bool expectedIsDisabled,
+        IEnumerable<DropdownMenuItem<int>> expectedMenuItems)
+    {
+        //Arrange
+        var expectedMarkup = BuildExpectedMarkup(expectedMenuItems, expectedIsDisabled);
+
+        //Act
+        var cut = RenderComponent<KebabMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.IsDisabled, expectedIsDisabled));
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+
+    [Fact(DisplayName = "Markup Test")]
+    public void KebabMenu_EmptyItems_Markup_RendersCorrectly()
+    {
+        //Arrange
+        var emptyItems = Enumerable.Empty<DropdownMenuItem<int>>();
+        var expectedMarkup = BuildExpectedMarkup(emptyItems, true);
+
+        //Act
+        var cut = RenderComponent<KebabMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, emptyItems)
+            .Add(p => p.IsDisabled, false));
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+
+    [Theory(DisplayName = "Click Test"), AutoData]
+    public void KebabMenu_Click_RendersCorrectly(
+        IEnumerable<DropdownMenuItem<int>> expectedMenuItems)
+    {
+        //Arrange
+        var expectedMarkup = BuildExpectedMarkup(expectedMenuItems, false);
+        var cut = RenderComponent<KebabMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.IsDisabled, false));
+        var optionsElement = cut.Find(".options");
+        var iElement = cut.Find("i");
+
+        //Act
+        iElement.Click();
+
+        //Assert
+        iElement.ClassList.ShouldNotContain("mdi-dots-vertical");
+        iElement.ClassList.ShouldContain("mdi-close-circle");
+        optionsElement.ClassList.ShouldContain("active");
+    }
+
+    [Theory(DisplayName = "Inactive Click Test"), AutoData]
+    public void KebabMenu_Inactive_Click_RendersCorrectly(
+       IEnumerable<DropdownMenuItem<int>> expectedMenuItems)
+    {
+        //Arrange
+        var expectedMarkup = BuildExpectedMarkup(expectedMenuItems, true);
+        var cut = RenderComponent<KebabMenu<int>>(parameters => parameters
+            .Add(p => p.MenuItems, expectedMenuItems)
+            .Add(p => p.IsDisabled, true));
+        var optionsElement = cut.Find(".options");
+        var iElement = cut.Find("i");
+
+        //Act
+        iElement.Click();
+
+        //Assert
+        iElement.ClassList.ShouldContain("mdi-dots-vertical");
+        iElement.ClassList.ShouldNotContain("mdi-close-circle");
+        optionsElement.ClassList.ShouldNotContain("active");
+    }
+
+    [Theory(DisplayName = "MenuItems Parameter Callback Test"), AutoData]
+    public void Kebab_MenuItemsParameterCallback_RendersCorrectly(
+        IEnumerable<DropdownMenuItem<int>> expectedMenuItems)
+    {
+        //Arrange
+        var selectedIndex = RandomUtilities.GetRandomIndex(expectedMenuItems.Count());
+        var callbackFired = false;
+        var callbackIndex = -1;
+        var expectedItemsWithCallback = expectedMenuItems.Select((item, i) =>
+            item with
+            {
+                MenuItemSelected = () =>
+                {
+                    callbackFired = true;
+                    callbackIndex = i;
+                }
+            });
+
+        var cut = RenderComponent<KebabMenu<int>>(parameters => parameters
+             .Add(p => p.MenuItems, expectedItemsWithCallback)
+             .Add(p => p.IsDisabled, false));
+
+        //Act
+        cut.FindAll(".option").ElementAt(selectedIndex).Click();
+
+        //Assert
+        callbackFired.ShouldBeTrue();
+        callbackIndex.ShouldBe(selectedIndex);
+    }
+
+    private static string BuildExpectedMarkup(IEnumerable<DropdownMenuItem<int>> items, bool isDisabled)
+    {
+        var optionsMarkup = string.Join(Environment.NewLine, items.Select(item =>
+          $@"<div class=""option"">{item.MenuItemName}</div>"
+        ));
+
+        return
+        @$"
+        <div class=""kebab-dropdown"">
+            <i class=""{(isDisabled ? "disabled" : string.Empty )} mdi mdi-24px mdi-dots-vertical""  ></i>
+            <div class=""options"">
+                {optionsMarkup}
+            </div>
+        </div>";
+    }
+}
