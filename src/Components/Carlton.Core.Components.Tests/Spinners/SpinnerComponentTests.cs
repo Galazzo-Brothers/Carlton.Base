e52815e1c64dbbcd93988d94228881407f656335
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-06 10:12:52 -0500

    added unit tests for spinner horizontalAccentDivider

---
+++ src/Components/Carlton.Core.Components.Tests/Spinners/SpinnerComponentTests.cs
@@ -0,0 +1,20 @@
+﻿using Carlton.Core.Components.Spinners;
+namespace Carlton.Core.Components.Tests.Spinners;
+
+
+[Trait("Component", nameof(Spinner))]
+public class SpinnerComponentTests : TestContext
+{
+    [Fact(DisplayName = "Markup Test")]
+    public void Spinner_Markup_RendersCorrectly()
+    {
+        //Arrange
+        var expectedMarkup = @$"<div class=""spinner-container""><div class=""spinner""></div></div>";
+
+        //Act
+        var cut = RenderComponent<Spinner>();
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+}
