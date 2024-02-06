5d7afb6f668de7aa3658233b1051cb43d0a6ea8a
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-05 21:13:58 -0500

    ProfileAvatarDropdown tests

---
+++ src/Components/Carlton.Core.Components.Tests/Dropdowns/ProfileAvatarDropdownComponentTests.cs
@@ -0,0 +1,63 @@
+﻿using Carlton.Core.Components.Dropdowns;
+namespace Carlton.Core.Components.Tests;
+
+
+[Trait("Component", nameof(ProfileAvatarDropdown))]
+public class ProfileAvatarDropdownComponentTests : TestContext
+{
+    [Theory(DisplayName = "Markup Test"), AutoData]
+    public void ProfileAvatar_Markup_RendersCorrectly(
+        string expectedAvatarUrl,
+        string expectedUserName,
+        IEnumerable<DropdownMenuItem<int>> expectedMenuItems)
+    {
+        //Arrange
+        var expectedMarkup = BuildExpectedMarkup(expectedAvatarUrl, expectedUserName, expectedMenuItems);
+
+        //Act
+        var cut = RenderComponent<ProfileAvatarDropdown>(parameters => parameters
+                    .Add(p => p.AvatarImgUrl, expectedAvatarUrl)
+                    .Add(p => p.Username, expectedUserName)
+                    .Add(p => p.DropdownMenuItems, expectedMenuItems));
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+
+    //TODO: Add Additiona Tests
+
+    private static string BuildExpectedMarkup(string expectedAvatarUrl, string expectedUserName, IEnumerable<DropdownMenuItem<int>> menuItems)
+    {
+        var itemsMarkup = string.Join(Environment.NewLine, menuItems.Select(item => $@"
+        <li>
+          <span class=""mdi mdi-{item.MenuIcon} accent-color-{item.AccentColorIndex}""></span>
+          <a href = ""#"">{item.MenuItemName}</a>
+        </li>"));
+
+
+        var markup = $@"
+        <div class="" profile-avatar-dropdown"">
+            <div class=""dropdown-menu"">
+                <div class=""menu-template"">
+                    <div class=""profile-avatar"">
+                        <img class=""avatar-img text-center"" src=""{expectedAvatarUrl}"">
+                        <span class=""username mobile-hide"">{expectedUserName}</span>
+                        <span class=""icon mdi mdi-24px mdi-chevron-down mobile-hide""></span>
+                    </div>
+                </div>
+                <div class=""dropdown-items"">
+                  <div class=""header-template"">
+                    <div class=""header"">
+                      <span class=""user"">Stephen Galazzo</span>
+                      <span class=""email"">Admin User</span>
+                      <span class=""horizontal-spacer"" style="" --spacer-width:85%;--spacer-height:1px;""></span>
+                    </div>
+                  </div>
+                  <ul> {itemsMarkup} </ul>
+                </div>
+              </div>
+            </div>";
+
+        return markup;
+    }
+}
---
+++ src/Components/Carlton.Core.Components.Tests/Pill/PillComponentTests.cs
@@ -0,0 +1,20 @@
+﻿using Carlton.Core.Components.Pills;
+namespace Carlton.Core.Components.Tests;
+
+[Trait("Component", nameof(Pill))]
+public class PillComponentTests : TestContext
+{
+    [Theory(DisplayName = "Markup Test"), AutoData]
+    public void Pill_Markup_RendersCorrectly(string expectedText)
+    {
+        //Arrange
+        var expectedMarkup = @$"<div class=""pill"">{expectedText}</div>";
+
+        //Act
+        var cut = RenderComponent<Pill>(parameters => parameters
+                    .Add(p => p.Text, expectedText));
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+}
