6d65de733f90cbbfe420c77525e704b79d3a104d
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-08 00:23:00 -0500

    refactor the way modals work in the layout manager

--- src/Components/Carlton.Core.Components.Layouts/State/Modals/IModalState.cs
+++ src/Components/Carlton.Core.Components.Layouts/State/Modals/IModalState.cs
@@ -3,12 +3,11 @@ namespace Carlton.Core.Components.Layouts.State.Modals;
 
 public interface IModalState
 {
-    public event EventHandler<ModalRaisedEventArgs> ModalRaised;
+    public event EventHandler<ModalStateChangedEventArgs> ModalStateChanged;
 
+    public bool IsVisible { get; }
+    public ModalTypes ModalType { get; }
     public ModalViewModel ModalModel { get; }
-    public Type ModalType { get; }
-    public void RaiseModal<TModal>(string modalPrompt, string modalMessage)
-        where TModal : Modal;
-    public void RaiseModal<TModal>(string modalPrompt, string modalMessage, Func<Task> modalDismissedFunc, Func<object, Task> modelClosedFunc)
-        where TModal : Modal;
+
+    public void RaiseModal(ModalTypes modalType, ModalViewModel modalModel);
 }
---
+++ src/Components/Carlton.Core.Components/Modals/ModalTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Carlton.Core.Components.Modals;
+
+public enum ModalTypes
+{
+    SingleActionModal = 1,
+    ConfirmationModal = 2,
+}
