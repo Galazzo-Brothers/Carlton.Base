667ff20684a54278a9fa87c1696a3d7472dda573
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-09 16:51:16 -0500

    changed to the way toasts work

--- src/Components/Carlton.Core.Components/Toasts/ToastViewModel.cs
+++ src/Components/Carlton.Core.Components/Toasts/ToastViewModel.cs
@@ -1,10 +1,15 @@
 ﻿namespace Carlton.Core.Components.Toasts;
 
-public record ToastViewModel(int ToastIndex, string Title, string Message, ToastTypes ToastType)
+public class ToastViewModel(int ToastIndex, string Title, string Message, ToastTypes ToastType)
 {
     public string Title { get; init; } = Title;
     public string Message { get; init; } = Message;
     public ToastTypes ToastType { get; init; } = ToastType;
     public bool FadeOutEnabled { get; init; } = true;
     public int ToastIndex { get; init; } = ToastIndex;
+
+    public bool Toasted { get; private set; } = false;
+
+    public void MarkAsToasted()
+     => Toasted = true;
 }
