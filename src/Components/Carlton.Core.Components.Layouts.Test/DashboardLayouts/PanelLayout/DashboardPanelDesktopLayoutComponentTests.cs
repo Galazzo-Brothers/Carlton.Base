f3a2f0a3e60189653084151a58037e9b40e3727d
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-02-26 09:22:49 -0500

    added reamining unit tests for Carlton.Components.Layouts

---
+++ src/Components/Carlton.Core.Components.Layouts.Test/DashboardLayouts/PanelLayout/DashboardPanelDesktopLayoutComponentTests.cs
@@ -0,0 +1,70 @@
+﻿using Carlton.Core.Components.Layouts.DashboardLayouts.PanelLayout;
+using Carlton.Core.Components.Layouts.State.FullScreen;
+using Carlton.Core.Components.Panels;
+namespace Carlton.Core.Components.Layouts.Test.DashboardLayouts.PanelLayout;
+
+[Trait("Component", nameof(DashboardPanelLayout))]
+public class DashboardPanelDesktopLayoutComponentTests : TestContext
+{
+    [Theory(DisplayName = "Markup Test")]
+    [InlineAutoData(true)]
+    [InlineAutoData(false)]
+    public void DashboardPanelDesktopLayout_Markup_RendersCorrectly(
+        bool expectedIsFullScreen,
+        string expectedAppName,
+        string expectedLogoUrl,
+        string expectedContent)
+    {
+        //Arrange
+        var moduleInterop = JSInterop.SetupModule(ResizablePanel.ImportPath);
+        moduleInterop.SetupVoid(ResizablePanel.InitResizablePanel);
+        var navStateMock = Substitute.For<IFullScreenState>();
+        Services.AddSingleton(navStateMock);
+
+        var expectedMarkup = $@" <div class=""layout-container {(expectedIsFullScreen ? "collapsed" : string.Empty)}"" >
+          <header >
+            <div >
+              <div class=""header-bar"" >
+                <div class=""menu"" >
+                  <div class=""collapser"" >
+                    <a href=""#""   >
+                      <span class=""mdi mdi-24px mdi-menu"" ></span>
+                    </a>
+                  </div>
+                </div>
+                <div class=""title"" ></div>
+                <div class=""action"" ></div>
+              </div>
+              <span class=""horizontal-spacer"" style=""--spacer-width:100%;--spacer-height:1px;"" ></span>
+            </div>
+          </header>
+          <nav >
+            <div class=""logo {(expectedIsFullScreen ? "collapsed" : string.Empty)}"" >
+                <div class=""content"">
+                    <img src=""{expectedLogoUrl}"">
+                    <span class=""title"">{expectedAppName}</span>
+                </div>              
+            </div>
+          </nav>
+          <main >
+            <div class=""resizable-panel"" >
+              <div class=""panel-top"" >{expectedContent}</div>
+              <div class=""splitter-horizontal"" ></div>
+              <div class=""panel-bottom"" ></div>
+            </div>
+          </main>
+          <footer ></footer>
+        </div>";
+
+        //Act
+        var cut = RenderComponent<DashboardPanelDesktopLayout>(parameters =>
+            parameters.Add(p => p.BodyContent, expectedContent)
+                      .Add(p => p.AppName, expectedAppName)
+                      .Add(p => p.LogoUrl, expectedLogoUrl)
+                      .Add(p => p.IsFullScreen, expectedIsFullScreen));
+
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+}
---
+++ src/Components/Carlton.Core.Components.Layouts.Test/DashboardLayouts/PanelLayout/DashboardPanelLayoutComponentTests.cs
@@ -0,0 +1,158 @@
+﻿using Bunit.TestDoubles;
+using Carlton.Core.Components.Layouts.DashboardLayouts.PanelLayout;
+using Carlton.Core.Components.Layouts.State.Viewport;
+namespace Carlton.Core.Components.Layouts.Test.DashboardLayouts.PanelLayout;
+
+[Trait("Component", nameof(DashboardPanelLayout))]
+public class DashboardPanelLayoutComponentTests : TestContext
+{
+    [Theory(DisplayName = "Markup Test")]
+    [InlineAutoData(1000)]
+    [InlineAutoData(500)]
+    public void DashboardPanelLayout_Markup_RendersCorrectly(
+        double expectedViewportWidth,
+        bool expectedIsFullScreen,
+        string expectedAppName,
+        string expectedLogoUrl,
+        bool expectedShowPanel)
+    {
+        //Arrange
+        var viewportStateMock = Substitute.For<IViewportState>();
+        var expectedViewportState = new ViewportModel(10, expectedViewportWidth);
+        viewportStateMock.GetCurrentViewport().Returns(Task.FromResult(expectedViewportState));
+        Services.AddSingleton(viewportStateMock);
+        var state = new LayoutManagerCascadingState
+        {
+            IsFullScreen = expectedIsFullScreen,
+            LayoutSettings = new Dictionary<string, object>
+            {
+                {"AppName", expectedAppName },
+                { "LogoUrl",  expectedLogoUrl },
+                { "ShowPanel", expectedShowPanel }
+            }
+        };
+        ComponentFactories.AddStub<DashboardPanelDesktopLayout>("<div>Desktop Content</div>");
+        ComponentFactories.AddStub<DashboardPanelMobileLayout>("<div>Mobile Content</div>");
+
+        var expectedMarkup = $@"
+        <div class=""media-query-wrapper"" >
+          <div class=""{(expectedViewportState.IsMobile ? "mobile" : "desktop")}"" >
+            <div>{(expectedViewportState.IsMobile ? "Mobile Content" : "Desktop Content")}</div>
+          </div>
+        </div>";
+
+        //Act
+        var cut = RenderComponent<DashboardPanelLayout>(parameters => parameters.AddCascadingValue(state));
+
+        //Assert
+        cut.MarkupMatches(expectedMarkup);
+    }
+
+    [Theory(DisplayName = "Cascading Parameter Desktop Test"), AutoData]
+    public void DashboardPanelLayout_CascadingParameter_Desktop_RendersCorrectly(
+       bool expectedIsFullScreen,
+       string expectedAppName,
+       string expectedLogoUrl,
+       bool expectedShowPanel)
+    {
+        //Arrange
+        var viewportStateMock = Substitute.For<IViewportState>();
+        var expectedViewportState = new ViewportModel(10, 1000);
+        viewportStateMock.GetCurrentViewport().Returns(Task.FromResult(expectedViewportState));
+        viewportStateMock.ViewportChanged += (sender, args) => { };
+        Services.AddSingleton(viewportStateMock);
+        var state = new LayoutManagerCascadingState
+        {
+            IsFullScreen = expectedIsFullScreen,
+            LayoutSettings = new Dictionary<string, object>
+            {
+                {"AppName", expectedAppName },
+                { "LogoUrl",  expectedLogoUrl },
+                { "ShowPanel", expectedShowPanel }
+            }
+        };
+        ComponentFactories.AddStub<DashboardPanelDesktopLayout>("<div>Desktop Content</div>");
+        ComponentFactories.AddStub<DashboardPanelMobileLayout>("<div>Mobile Content</div>");
+
+        //Act
+        var cut = RenderComponent<DashboardPanelLayout>(parameters => parameters.AddCascadingValue(state));
+        var stub = cut.FindComponent<Stub<DashboardPanelDesktopLayout>>();
+
+        //Assert
+        stub.Instance.Parameters.Get(_ => _.AppName).ShouldBe(expectedAppName);
+        stub.Instance.Parameters.Get(_ => _.LogoUrl).ShouldBe(expectedLogoUrl);
+        stub.Instance.Parameters.Get(_ => _.IsFullScreen).ShouldBe(expectedIsFullScreen);
+    }
+
+    [Theory(DisplayName = "Cascading Parameter Mobile Test"), AutoData]
+    public void DashboardPanelLayout_CascadingParameter_Mobile_RendersCorrectly(
+     bool expectedIsFullScreen,
+     string expectedAppName,
+     string expectedLogoUrl,
+     bool expectedShowPanel)
+    {
+        //Arrange
+        var viewportStateMock = Substitute.For<IViewportState>();
+        var expectedViewportState = new ViewportModel(10, 500);
+        viewportStateMock.GetCurrentViewport().Returns(Task.FromResult(expectedViewportState));
+        viewportStateMock.ViewportChanged += (sender, args) => { };
+        Services.AddSingleton(viewportStateMock);
+        var state = new LayoutManagerCascadingState
+        {
+            IsFullScreen = expectedIsFullScreen,
+            LayoutSettings = new Dictionary<string, object>
+            {
+                {"AppName", expectedAppName },
+                { "LogoUrl",  expectedLogoUrl },
+                { "ShowPanel", expectedShowPanel }
+            }
+        };
+        ComponentFactories.AddStub<DashboardPanelDesktopLayout>("<div>Desktop Content</div>");
+        ComponentFactories.AddStub<DashboardPanelMobileLayout>("<div>Mobile Content</div>");
+
+        //Act
+        var cut = RenderComponent<DashboardPanelLayout>(parameters => parameters.AddCascadingValue(state));
+        var stub = cut.FindComponent<Stub<DashboardPanelMobileLayout>>();
+
+        //Assert
+        stub.Instance.Parameters.Get(_ => _.LogoUrl).ShouldBe(expectedLogoUrl);
+        stub.Instance.Parameters.Get(_ => _.IsFullScreen).ShouldBe(expectedIsFullScreen);
+    }
+
+    [Theory(DisplayName = "Cascading Parameter Property Test"), AutoData]
+    public void DashboardPanelLayout_CascadingParameter_ShouldSetProperties(
+        bool expectedIsFullScreen,
+        string expectedAppName,
+        string expectedLogoUrl,
+        bool expectedShowPanel)
+    {
+        //Arrange
+        var viewportStateMock = Substitute.For<IViewportState>();
+        var expectedViewportState = new ViewportModel(10, 500);
+        viewportStateMock.GetCurrentViewport().Returns(Task.FromResult(expectedViewportState));
+        viewportStateMock.ViewportChanged += (sender, args) => { };
+        Services.AddSingleton(viewportStateMock);
+        var state = new LayoutManagerCascadingState
+        {
+            IsFullScreen = expectedIsFullScreen,
+            LayoutSettings = new Dictionary<string, object>
+            {
+                {"AppName", expectedAppName },
+                { "LogoUrl",  expectedLogoUrl },
+                { "ShowPanel", expectedShowPanel }
+            }
+        };
+        ComponentFactories.AddStub<DashboardPanelDesktopLayout>("<div>Desktop Content</div>");
+        ComponentFactories.AddStub<DashboardPanelMobileLayout>("<div>Mobile Content</div>");
+
+        //Act
+        var cut = RenderComponent<DashboardPanelLayout>(parameters => parameters.AddCascadingValue(state));
+        var stub = cut.FindComponent<Stub<DashboardPanelMobileLayout>>();
+
+        //Assert
+        cut.Instance.AppName.ShouldBe(expectedAppName);
+        cut.Instance.LogoUrl.ShouldBe(expectedLogoUrl);
+        cut.Instance.ShowPanel.ShouldBe(expectedShowPanel);
+    }
+}
+
