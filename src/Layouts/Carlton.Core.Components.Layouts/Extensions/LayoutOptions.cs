c3206da119fdfd139a0c0ac74392fa1ebefee89c
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-01 12:35:38 -0500

    updated default layout options, Dashboard Layout now uses LayoutManger as a layout and Dashboard PanelLayout uses DashboardLayout

--- src/Layouts/Carlton.Core.Components.Layouts/Extensions/LayoutOptions.cs
+++ src/Layouts/Carlton.Core.Components.Layouts/Extensions/LayoutOptions.cs
@@ -10,15 +10,15 @@ public class LayoutOptions
     /// <summary>
     /// Gets or Sets a value indicating the default theme of the layout.
     /// </summary>
-    public Themes Theme { get; set; }
+    public Themes Theme { get; set; } = Themes.light;
 
     /// <summary>
     /// Gets or Sets a value indicating whether the layout is in full-screen mode.
     /// </summary>
-    public bool IsFullScreen { get; set; }
+    public bool IsFullScreen { get; set; } = false;
 
     /// <summary>
     /// Gets or Sets a value indicating whether the layout panel content is visible.
     /// </summary>
-    public bool ShowPanel { get; set; }
+    public bool ShowPanel { get; set; } = true;
 }
