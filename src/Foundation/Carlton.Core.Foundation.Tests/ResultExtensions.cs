99a27db2306ee9c3ad49b1044ab401b11e9fd86f
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-08 13:39:41 -0500

    added some new testing utilities

--- src/Foundation/Carlton.Core.Foundation.Tests/ResultExtensions.cs
+++ src/Foundation/Carlton.Core.Foundation.Tests/ResultExtensions.cs
@@ -7,12 +7,12 @@ public static class ResultExtensions
     public static TValue GetValue<TValue, TError>(this Result<TValue, TError> result)
     {
         var fieldInfo = result.GetType().GetField("_value", BindingFlags.Instance | BindingFlags.NonPublic);
-        return (TValue) fieldInfo.GetValue(result);
+        return (TValue) fieldInfo.GetValue(result)!;
     }
 
     public static TError GetError<TValue, TError>(this Result<TValue, TError> result)
     {
         var fieldInfo = result.GetType().GetField("_error", BindingFlags.Instance | BindingFlags.NonPublic);
-        return (TError)fieldInfo.GetValue(result);
+        return (TError)fieldInfo.GetValue(result)!;
     }
 }
