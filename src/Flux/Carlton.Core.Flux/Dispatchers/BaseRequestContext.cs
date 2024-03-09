8f4cb84b0f3f5c0826c8377b31995a3c8fd097eb
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 12:42:47 -0500

    exception decorators now explicitly mark the request as started

--- src/Flux/Carlton.Core.Flux/Dispatchers/BaseRequestContext.cs
+++ src/Flux/Carlton.Core.Flux/Dispatchers/BaseRequestContext.cs
@@ -4,42 +4,44 @@ namespace Carlton.Core.Flux.Dispatchers;
 
 public abstract class BaseRequestContext
 {
-    private readonly Stopwatch _stopwatch = new();
+	private readonly Stopwatch _stopwatch = new();
 
-    //Common
-    public Guid RequestId { get; } = Guid.NewGuid();
-    public abstract FluxOperation FluxOperation { get; }
-    public abstract Type FluxOperationType { get; }
-    public string FluxOperationTypeName { get => FluxOperationType.GetDisplayName(); }
-    
-
-    //Http Context
-    public bool RequiresHttpRefresh { get; private set; }
-    public bool HttpRefreshOccurred { get => RequestHttpContext != null; }
-    public RequestHttpContext RequestHttpContext { get; private set; }
-    internal void MarkAsRequiresHttpRefresh()
-       => RequiresHttpRefresh = true;
-    internal void MarkAsHttpCallMade(string httpUrl, HttpStatusCode httpStatusCode, object httpResponse)
-       => RequestHttpContext = new RequestHttpContext(httpUrl, httpStatusCode, httpResponse);
-
-    //Validation Result
-    public bool RequestValidated { get => ValidationResult != null; }
-    public ValidationResult ValidationResult { get; private set; }
-    internal void MarkAsValidated(IEnumerable<string> ValidationErrors)
-       => ValidationResult = new ValidationResult(ValidationErrors);
-    internal void MarkAsValidated()
-      => ValidationResult = new ValidationResult();
+	//Common
+	public Guid RequestId { get; } = Guid.NewGuid();
+	public abstract FluxOperation FluxOperation { get; }
+	public abstract Type FluxOperationType { get; }
+	public string FluxOperationTypeName { get => FluxOperationType.GetDisplayName(); }
+	protected internal void MarkAsStarted()
+		=> _stopwatch.Start();
 
 
-    //Request Result
-    public bool RequestInProgress { get => RequestResult == null; }
-    public RequestResult RequestResult { get; private set; }
-    protected internal void MarkAsSucceeded()
-        => RequestResult = new RequestResult(_stopwatch);
-    protected internal void MarkAsErrored(Exception exception)
-        => RequestResult = new RequestResult(Stopwatch.StartNew(), exception);
-    protected internal void MarkAsErrored(FluxError error)
-       => RequestResult = new RequestResult(Stopwatch.StartNew(), error);
+	//Http Context
+	public bool RequiresHttpRefresh { get; private set; }
+	public bool HttpRefreshOccurred { get => RequestHttpContext != null; }
+	public RequestHttpContext RequestHttpContext { get; private set; }
+	internal void MarkAsRequiresHttpRefresh()
+		=> RequiresHttpRefresh = true;
+	internal void MarkAsHttpCallMade(string httpUrl, HttpStatusCode httpStatusCode, object httpResponse)
+		=> RequestHttpContext = new RequestHttpContext(httpUrl, httpStatusCode, httpResponse);
+
+	//Validation Result
+	public bool RequestValidated { get => ValidationResult != null; }
+	public ValidationResult ValidationResult { get; private set; }
+	internal void MarkAsValidated(IEnumerable<string> ValidationErrors)
+		=> ValidationResult = new ValidationResult(ValidationErrors);
+	internal void MarkAsValidated()
+		=> ValidationResult = new ValidationResult();
+
+
+	//Request Result
+	public bool RequestInProgress { get => RequestResult == null; }
+	public RequestResult RequestResult { get; private set; }
+	protected internal void MarkAsSucceeded()
+		=> RequestResult = new RequestResult(_stopwatch);
+	protected internal void MarkAsErrored(Exception exception)
+		=> RequestResult = new RequestResult(_stopwatch, exception);
+	protected internal void MarkAsErrored(FluxError error)
+		=> RequestResult = new RequestResult(_stopwatch, error);
 }
 
 
@@ -47,42 +49,42 @@ public record RequestHttpContext(string HttpUrl, HttpStatusCode HttpStatusCode, 
 
 public record ValidationResult(IEnumerable<string> ValidationErrors)
 {
-    public bool ValidationPassed { get => !ValidationErrors.Any(); }
+	public bool ValidationPassed { get => !ValidationErrors.Any(); }
 
-    internal ValidationResult() : this(new List<string>())
-    { }
+	internal ValidationResult() : this(new List<string>())
+	{ }
 }
 
 public record RequestResult
 {
-    public FluxError FluxError { get; init; }
-    public bool RequestSucceeded { get => Exception == null && FluxError == null; }
-    public RequestExceptionContext Exception { get; init; }
-    public DateTimeOffset RequestEndTimestamp { get; init; }
-    public long ElapsedTime { get; init; }
+	public FluxError FluxError { get; init; }
+	public bool RequestSucceeded { get => Exception == null && FluxError == null; }
+	public RequestExceptionContext Exception { get; init; }
+	public DateTimeOffset RequestEndTimestamp { get; init; }
+	public long ElapsedTime { get; init; }
 
-    internal RequestResult(Stopwatch stopwatch, Exception exception) : this(stopwatch)
-    {
-        Exception = new RequestExceptionContext(exception.GetType().Name, exception.Message, exception.StackTrace);
-    }
+	internal RequestResult(Stopwatch stopwatch, Exception exception) : this(stopwatch)
+	{
+		Exception = new RequestExceptionContext(exception.GetType().Name, exception.Message, exception.StackTrace);
+	}
 
-    internal RequestResult(Stopwatch stopwatch, FluxError error) : this(stopwatch)
-    {
-        FluxError = error;
-    }
+	internal RequestResult(Stopwatch stopwatch, FluxError error) : this(stopwatch)
+	{
+		FluxError = error;
+	}
 
-    internal RequestResult(Stopwatch stopwatch)
-    {
-        RequestEndTimestamp = DateTimeOffset.UtcNow;
-        stopwatch.Stop();
-        ElapsedTime = stopwatch.ElapsedMilliseconds;
-    }
+	internal RequestResult(Stopwatch stopwatch)
+	{
+		RequestEndTimestamp = DateTimeOffset.UtcNow;
+		stopwatch.Stop();
+		ElapsedTime = stopwatch.ElapsedMilliseconds;
+	}
 }
 
 public record RequestExceptionContext(string ExceptionType, string Message, string StackTrace);
 
 public enum FluxOperation
 {
-    ViewModelQuery = 1,
-    CommandMutation = 2,
+	ViewModelQuery = 1,
+	CommandMutation = 2,
 }
