34b6291d86bbe285c949f80221d5fbb637489c41
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 12:46:59 -0500

    flux test clean up

--- src/Flux/Carlton.Core.Flux.Test/Dispatchers/ViewModels/ViewModelQueryHandlerTests.cs
+++ src/Flux/Carlton.Core.Flux.Test/Dispatchers/ViewModels/ViewModelQueryHandlerTests.cs
@@ -1,26 +1,50 @@
 ﻿using Carlton.Core.Flux.Contracts;
 using Carlton.Core.Flux.Dispatchers.ViewModels;
 using Carlton.Core.Foundation.Test;
+using NSubstitute.ExceptionExtensions;
 namespace Carlton.Core.Flux.Tests.Dispatchers.ViewModels;
 
 public class ViewModelQueryHandlerTests
 {
-    [Theory, AutoNSubstituteData]
-    public async Task Handle_ShouldCallStateMutation(
-       [Frozen] IFluxState<TestState> fluxState,
-       [Frozen] IViewModelMapper<TestState> mapper,
-       ViewModelQueryHandler<TestState> sut,
-       ViewModelQueryContext<TestViewModel> context,
-       TestViewModel expectedViewModel)
-    {
-        //Arrange
-        mapper.Map<TestViewModel>(fluxState.CurrentState).Returns(expectedViewModel);
+	[Theory, AutoNSubstituteData]
+	public async Task Handle_ShouldReturnViewModel(
+		[Frozen] IViewModelMapper<TestState> mapper,
+		[Frozen] IFluxState<TestState> fluxState,
+		ViewModelQueryHandler<TestState> sut,
+		TestState state,
+		ViewModelQueryContext<TestViewModel> context,
+		TestViewModel expectedViewModel)
+	{
+		//Arrange
+		fluxState.CurrentState.Returns(state);
+		mapper.Map<TestViewModel>(state).Returns(expectedViewModel);
 
-        //Act
-        var result = await sut.Handle(context, CancellationToken.None);
+		//Act
+		var result = await sut.Handle(context, CancellationToken.None);
 
-        //Assert
-        mapper.Received().Map<TestViewModel>(fluxState.CurrentState);
-        result.ShouldBe(expectedViewModel);
-    }
+		//Assert
+		mapper.Received().Map<TestViewModel>(state);
+		result.ShouldBe(expectedViewModel);
+	}
+
+	[Theory, AutoNSubstituteData]
+	public async Task Handle_WithMappingError_ShouldReturnError(
+		[Frozen] IViewModelMapper<TestState> mapper,
+		[Frozen] IFluxState<TestState> fluxState,
+		ViewModelQueryHandler<TestState> sut,
+		TestState state,
+		Exception ex,
+		ViewModelQueryContext<TestViewModel> context)
+	{
+		//Arrange
+		fluxState.CurrentState.Returns(state);
+		mapper.Map<TestViewModel>(state).Throws(ex);
+
+		//Act
+		var result = await sut.Handle(context, CancellationToken.None);
+
+		//Assert
+		mapper.Received().Map<TestViewModel>(state);
+		result.GetError().ShouldBeOfType<MappingError>();
+	}
 }
