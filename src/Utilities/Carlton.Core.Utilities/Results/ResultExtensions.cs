3c26be05178ccec2a55d611f09894d171d137d5c
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 12:41:34 -0500

    added some more result extensions

--- src/Utilities/Carlton.Core.Utilities/Results/ResultExtensions.cs
+++ src/Utilities/Carlton.Core.Utilities/Results/ResultExtensions.cs
@@ -2,53 +2,68 @@
 
 public static class ResultExtensions
 {
-    public static Result<TValue, TError> SafeExecute<TValue, TError>(
-        Func<Result<TValue, TError>> func,
-        Func<TValue, TValue> success,
-        Func<TError, TError> error,
-        Func<Exception, TError> exception)
-    {
-        try
-        {
-            return func().Match<Result<TValue, TError>>
-            (
-                val => success(val),
-                err => error(err)
-            );
-        }
-        catch (Exception ex)
-        {
-            return exception(ex);
-        }
-    }
+	public static Result<TValue, TError> SafeExecute<TValue, TError>(
+		Func<Result<TValue, TError>> func,
+		Func<TValue, TValue> success,
+		Func<TError, TError> error,
+		Func<Exception, TError> exception)
+	{
+		try
+		{
+			return func().Match<Result<TValue, TError>>
+			(
+				val => success(val),
+				err => error(err)
+			);
+		}
+		catch (Exception ex)
+		{
+			return exception(ex);
+		}
+	}
 
-    public static async Task<Result<TValue, TError>> SafeExecuteAsync<TValue, TError>(
-      Func<Task<Result<TValue, TError>>> func,
-      Func<TValue, TValue> success,
-      Func<TError, TError> error,
-      Func<Exception, TError> exception)
-    {
-        try
-        {
-            return (await func()).Match<Result<TValue, TError>>
-            (
-                val => success(val),
-                err => error(err)
-            );
-        }
-        catch (Exception ex)
-        {
-            return exception(ex);
-        }
-    }
+	public static async Task<Result<TValue, TError>> SafeExecuteAsync<TValue, TError>(
+		Func<Task<Result<TValue, TError>>> func,
+		Func<TValue, TValue> success,
+		Func<TError, TError> error,
+		Func<Exception, TError> exception)
+	{
+		try
+		{
+			return (await func()).Match<Result<TValue, TError>>
+			(
+				val => success(val),
+				err => error(err)
+			);
+		}
+		catch (Exception ex)
+		{
+			return exception(ex);
+		}
+	}
 
-    public static Task<Result<TValue, TError>> ToResultTask<TValue, TError>(this TError error)
-    {
-        return Task.FromResult<Result<TValue, TError>>(error);
-    }
+	public static Task<Result<TValue, TError>> ToResultTask<TValue, TError>(this TError error)
+	{
+		return Task.FromResult<Result<TValue, TError>>(error);
+	}
 
-    public static Result<TValue, TError> ToResult<TValue, TError>(this TError error)
-    {
-        return (Result<TValue, TError>)error;
-    }
+	public static Result<TValue, TError> ToResult<TValue, TError>(this TError error)
+	{
+		return (Result<TValue, TError>)error;
+	}
+
+	public static Result<TValue, TError> TryResult<TValue, TError, TException>(
+		Func<TValue> successFunc,
+		Func<TException, TError> exceptionFunc)
+		where TException : Exception
+	{
+		try
+		{
+			return successFunc();
+		}
+		catch (TException ex)
+		{
+			return exceptionFunc(ex);
+		}
+	}
 }
