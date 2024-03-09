53b14e50d9c2c1bf28c471dc42a90cedd6a0438c
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 12:42:47 -0500

    exception decorators now explicitly mark the request as started

--- src/Flux/Carlton.Core.Flux/Dispatchers/Mutations/Decorators/MutationExceptionDecorator.cs
+++ src/Flux/Carlton.Core.Flux/Dispatchers/Mutations/Decorators/MutationExceptionDecorator.cs
@@ -1,48 +1,50 @@
 ﻿namespace Carlton.Core.Flux.Dispatchers.Mutations.Decorators;
 
 public class MutationExceptionDecorator<TState>(
-    IMutationCommandDispatcher<TState> _decorated,
-    ILogger<MutationExceptionDecorator<TState>> _logger)
-    : IMutationCommandDispatcher<TState>
+	IMutationCommandDispatcher<TState> _decorated,
+	ILogger<MutationExceptionDecorator<TState>> _logger)
+	: IMutationCommandDispatcher<TState>
 {
-    public async Task<Result<MutationCommandResult, FluxError>> Dispatch<TCommand>(
-        object sender, MutationCommandContext<TCommand> context,
-        CancellationToken cancellationToken)
-    {
-        using (_logger.BeginMutationCommandRequestLoggingScopes(context))
-        {
-            return await ResultExtensions.SafeExecuteAsync
-            (
-                async () => await _decorated.Dispatch(sender, context, cancellationToken), //Wrapped Func
-                success => HandleSuccess(success, context), //Success Handler
-                err => HandleError(err, context), //Error Handler
-                ex => HandleException(ex, context) //Exception Handler
-            );
-        }
-    }
+	public async Task<Result<MutationCommandResult, FluxError>> Dispatch<TCommand>(
+		object sender, MutationCommandContext<TCommand> context,
+		CancellationToken cancellationToken)
+	{
+		using (_logger.BeginMutationCommandRequestLoggingScopes(context))
+		{
+			context.MarkAsStarted();
 
-    private MutationCommandResult HandleSuccess<TCommand>(MutationCommandResult result, MutationCommandContext<TCommand> context)
-    {
-        _logger.MutationCommandCompleted(context.FluxOperationTypeName);
-        return result;
-    }
+			return await ResultExtensions.SafeExecuteAsync
+			(
+				async () => await _decorated.Dispatch(sender, context, cancellationToken), //Wrapped Func
+				success => HandleSuccess(success, context), //Success Handler
+				err => HandleError(err, context), //Error Handler
+				ex => HandleException(ex, context) //Exception Handler
+			);
+		}
+	}
 
-    private FluxError HandleError<TCommand>(FluxError error, MutationCommandContext<TCommand> context)
-    {
-        context.MarkAsErrored(error);
-        using (_logger.BeginRequestErrorLoggingScopes(error.EventId))
-            _logger.MutationCommandErrored(context.FluxOperationTypeName);
+	private MutationCommandResult HandleSuccess<TCommand>(MutationCommandResult result, MutationCommandContext<TCommand> context)
+	{
+		_logger.MutationCommandCompleted(context.FluxOperationTypeName);
+		return result;
+	}
 
-        return error;
-    }
+	private FluxError HandleError<TCommand>(FluxError error, MutationCommandContext<TCommand> context)
+	{
+		context.MarkAsErrored(error);
+		using (_logger.BeginRequestErrorLoggingScopes(error.EventId))
+			_logger.MutationCommandErrored(context.FluxOperationTypeName);
 
-    private UnhandledFluxError HandleException<TCommand>(Exception ex, MutationCommandContext<TCommand> context)
-    {
-        context.MarkAsErrored(ex);
-        using (_logger.BeginRequestErrorLoggingScopes(FluxLogs.Flux_Unhandled_Error))
-            _logger.MutationCommandErrored(context.FluxOperationTypeName, ex);
+		return error;
+	}
 
-        return UnhandledFluxError(ex);
-    }
+	private UnhandledFluxError HandleException<TCommand>(Exception ex, MutationCommandContext<TCommand> context)
+	{
+		context.MarkAsErrored(ex);
+		using (_logger.BeginRequestErrorLoggingScopes(FluxLogs.Flux_Unhandled_Error))
+			_logger.MutationCommandErrored(context.FluxOperationTypeName, ex);
+
+		return UnhandledFluxError(ex);
+	}
 }
 
--- src/Flux/Carlton.Core.Flux/Dispatchers/ViewModels/Decorators/ViewModelExceptionDecorator.cs
+++ src/Flux/Carlton.Core.Flux/Dispatchers/ViewModels/Decorators/ViewModelExceptionDecorator.cs
@@ -1,45 +1,47 @@
 ﻿namespace Carlton.Core.Flux.Dispatchers.ViewModels.Decorators;
 
 public class ViewModelExceptionDecorator<TState>(
-    IViewModelQueryDispatcher<TState> _decorated,
-    ILogger<ViewModelExceptionDecorator<TState>> _logger)
-    : IViewModelQueryDispatcher<TState>
+	IViewModelQueryDispatcher<TState> _decorated,
+	ILogger<ViewModelExceptionDecorator<TState>> _logger)
+	: IViewModelQueryDispatcher<TState>
 {
-    public async Task<Result<TViewModel, FluxError>> Dispatch<TViewModel>(object sender, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
-    {
-        using (_logger.BeginViewModelRequestLoggingScopes(context))
-        {
-            return await ResultExtensions.SafeExecuteAsync
-            (
-                async () => await _decorated.Dispatch(sender, context, cancellationToken), //Progress through the pipeline wrapped in a try/catch
-                success => HandleSuccess(success, context), //Success Handler
-                err => HandleError(err, context), //Error Handler
-                ex => HandleException(ex, context) //Exception handler
-            );
-        }
-    }
+	public async Task<Result<TViewModel, FluxError>> Dispatch<TViewModel>(object sender, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
+	{
+		using (_logger.BeginViewModelRequestLoggingScopes(context))
+		{
+			context.MarkAsStarted();
 
-    private TViewModel HandleSuccess<TViewModel>(TViewModel vm, ViewModelQueryContext<TViewModel> context)
-    {
-        _logger.ViewModelQueryCompleted(context.FluxOperationTypeName);
-        return vm;
-    }
+			return await ResultExtensions.SafeExecuteAsync
+			(
+				async () => await _decorated.Dispatch(sender, context, cancellationToken), //Progress through the pipeline wrapped in a try/catch
+				success => HandleSuccess(success, context), //Success Handler
+				err => HandleError(err, context), //Error Handler
+				ex => HandleException(ex, context) //Exception handler
+			);
+		}
+	}
 
-    private FluxError HandleError<TViewModel>(FluxError error, ViewModelQueryContext<TViewModel> context)
-    {
-        context.MarkAsErrored(error);
-        using (_logger.BeginRequestErrorLoggingScopes(error))
-            _logger.ViewModelQueryErrored(context.FluxOperationTypeName);
+	private TViewModel HandleSuccess<TViewModel>(TViewModel vm, ViewModelQueryContext<TViewModel> context)
+	{
+		_logger.ViewModelQueryCompleted(context.FluxOperationTypeName);
+		return vm;
+	}
 
-        return error;
-    }
+	private FluxError HandleError<TViewModel>(FluxError error, ViewModelQueryContext<TViewModel> context)
+	{
+		context.MarkAsErrored(error);
+		using (_logger.BeginRequestErrorLoggingScopes(error))
+			_logger.ViewModelQueryErrored(context.FluxOperationTypeName);
 
-    private UnhandledFluxError HandleException<TViewModel>(Exception ex, ViewModelQueryContext<TViewModel> context)
-    {
-        context.MarkAsErrored(ex);
-        using (_logger.BeginRequestErrorLoggingScopes(FluxLogs.Flux_Unhandled_Error))
-            _logger.ViewModelQueryErrored(context.FluxOperationTypeName, ex);
+		return error;
+	}
 
-        return UnhandledFluxError(ex);
-    }
+	private UnhandledFluxError HandleException<TViewModel>(Exception ex, ViewModelQueryContext<TViewModel> context)
+	{
+		context.MarkAsErrored(ex);
+		using (_logger.BeginRequestErrorLoggingScopes(FluxLogs.Flux_Unhandled_Error))
+			_logger.ViewModelQueryErrored(context.FluxOperationTypeName, ex);
+
+		return UnhandledFluxError(ex);
+	}
 }
