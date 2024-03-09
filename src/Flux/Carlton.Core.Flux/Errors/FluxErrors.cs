8a655560f8ce048ae6d3f25242a85cd0c8103291
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 12:45:51 -0500

    error message tweaks

--- src/Flux/Carlton.Core.Flux/Errors/FluxErrors.cs
+++ src/Flux/Carlton.Core.Flux/Errors/FluxErrors.cs
@@ -3,74 +3,77 @@ namespace Carlton.Core.Flux.Errors;
 
 public static class FluxErrors
 {
-    public abstract record FluxError(string Message, int EventId);
+	public abstract record FluxError(string Message, int EventId);
 
-    public static ValidationError ValidationError(IEnumerable<string> validationErrors)
-        => new(validationErrors);
+	public static ValidationError ValidationError(IEnumerable<string> validationErrors)
+		=> new(validationErrors);
 
-    public static UnsupportedHttpVerbError UnsupportedHttpVerbError(HttpVerb httpVerb)
-        => new(httpVerb);
+	public static UnsupportedHttpVerbError UnsupportedHttpVerbError(HttpVerb httpVerb)
+		=> new(httpVerb);
 
-    public static HttpUrlConstructionError HttpUrlConstructionError(string url)
-        => new(url);
+	public static HttpUrlConstructionError HttpUrlConstructionError(string url)
+		=> new(url);
 
-    public static HttpUrlConstructionUnreplacedTokensError HttpUrlConstructionUnreplacedTokensError(string url, IEnumerable<string> unreplacedTokens)
-        => new(url, unreplacedTokens);
+	public static HttpUrlConstructionUnreplacedTokensError HttpUrlConstructionUnreplacedTokensError(string url, IEnumerable<string> unreplacedTokens)
+		=> new(url, unreplacedTokens);
 
-    public static HttpRequestFailedError HttpRequestFailedError(HttpResponseMessage httpResponse)
-        => new(httpResponse);
+	public static HttpRequestFailedError HttpRequestFailedError(HttpResponseMessage httpResponse)
+		=> new(httpResponse);
 
-    public static HttpError HttpError(HttpRequestException exception)
-        => new(exception);
+	public static HttpError HttpError(HttpRequestException exception)
+		=> new(exception);
 
-    public static JsonError JsonError(Exception exception)
-        => new(exception);
+	public static JsonError JsonError(Exception exception)
+		=> new(exception);
 
-    public static MutationNotRegisteredError MutationNotRegisteredError(string mutationType)
-        => new(mutationType);
+	public static MutationNotRegisteredError MutationNotRegisteredError(string mutationType)
+		=> new(mutationType);
 
-    public static MutationError MutationError(object mutationCommand, Exception exception)
-        => new(mutationCommand, exception);
+	public static MutationError MutationError(string mutationCommandType, Exception exception)
+		=> new(mutationCommandType, exception);
 
-    public static UnhandledFluxError UnhandledFluxError(Exception exception)
-        => new(exception);
+	public static UnhandledFluxError UnhandledFluxError(Exception exception)
+		=> new(exception);
 
-    internal static string ReplaceHttpRequestFailedTemplateMessage(HttpResponseMessage httpResponse)
-    {
-        return string.Format(FluxLogs.Flux_HTTP_FailedRequest_ErrorMsg,
-                httpResponse.RequestMessage.Method,
-                httpResponse.RequestMessage.RequestUri,
-                (int)httpResponse.StatusCode,
-                httpResponse.ReasonPhrase);
-    }
+	internal static string ReplaceHttpRequestFailedTemplateMessage(HttpResponseMessage httpResponse)
+	{
+		return string.Format(FluxLogs.Flux_HTTP_FailedRequest_ErrorMsg,
+				httpResponse.RequestMessage.Method,
+				httpResponse.RequestMessage.RequestUri,
+				(int)httpResponse.StatusCode,
+				httpResponse.ReasonPhrase);
+	}
 }
 
 public sealed record ValidationError(IEnumerable<string> ValidationErrors)
-       : FluxError($"{FluxLogs.Flux_Validation_ErrorMsg}: {string.Join(Environment.NewLine, ValidationErrors)}", FluxLogs.Flux_Validation_Error);
+	: FluxError($"{FluxLogs.Flux_Validation_ErrorMsg}: {string.Join(Environment.NewLine, ValidationErrors)}", FluxLogs.Flux_Validation_Error);
 
 public sealed record UnsupportedHttpVerbError(HttpVerb HttpVerb)
-    : FluxError(string.Format(FluxLogs.Flux_HTTP_UnsupportedVerb_ErrorMsg, HttpVerb), FluxLogs.Flux_HTTP_UnsupportedVerb_Error);
+	: FluxError(string.Format(FluxLogs.Flux_HTTP_UnsupportedVerb_ErrorMsg, HttpVerb), FluxLogs.Flux_HTTP_UnsupportedVerb_Error);
 
 public sealed record HttpUrlConstructionError(string Url)
-   : FluxError(string.Format(FluxLogs.Flux_HTTP_Invalid_URL_ErrorMsg, Url), FluxLogs.Flux_HTTP_Invalid_URL_Error);
+	: FluxError(string.Format(FluxLogs.Flux_HTTP_Invalid_URL_ErrorMsg, Url), FluxLogs.Flux_HTTP_Invalid_URL_Error);
 
 public sealed record HttpUrlConstructionUnreplacedTokensError(string Url, IEnumerable<string> UnreplacedTokens)
-  : FluxError($"{FluxLogs.Flux_HTTP_Invalid_URL_UnreplacedTokens_ErrorMsg}: {string.Join(Environment.NewLine, UnreplacedTokens)}", FluxLogs.Flux_HTTP_Invalid_URL_UnreplacedTokens_Error);
+	: FluxError($"{FluxLogs.Flux_HTTP_Invalid_URL_UnreplacedTokens_ErrorMsg}: {string.Join(Environment.NewLine, UnreplacedTokens)}", FluxLogs.Flux_HTTP_Invalid_URL_UnreplacedTokens_Error);
 
 public sealed record HttpRequestFailedError(HttpResponseMessage HttpResponse)
-    : FluxError(ReplaceHttpRequestFailedTemplateMessage(HttpResponse), FluxLogs.Flux_HTTP_FailedRequest_Error);
+	: FluxError(ReplaceHttpRequestFailedTemplateMessage(HttpResponse), FluxLogs.Flux_HTTP_FailedRequest_Error);
 
 public sealed record HttpError(HttpRequestException Exception)
-   : FluxError($"{FluxLogs.Flux_HTTP_ErrorMsg}: {Exception.Message}.", FluxLogs.Flux_HTTP_Error);
+	: FluxError($"{FluxLogs.Flux_HTTP_ErrorMsg}: {Exception.Message}.", FluxLogs.Flux_HTTP_Error);
 
 public sealed record JsonError(Exception Exception)
-  : FluxError($"{FluxLogs.Flux_JSON_ErrorMsg}: {Exception.Message}.", FluxLogs.Flux_JSON_Error);
+	: FluxError($"{FluxLogs.Flux_JSON_ErrorMsg}: {Exception.Message}.", FluxLogs.Flux_JSON_Error);
 
 public sealed record MutationNotRegisteredError(string MutationType)
-  : FluxError($"{FluxLogs.Flux_MutationNotRegistered_ErrorMsg}: {MutationType}.", FluxLogs.Flux_MutationNotRegistered_Error);
+	: FluxError(string.Format(FluxLogs.Flux_MutationNotRegistered_ErrorMsg, MutationType), FluxLogs.Flux_MutationNotRegistered_Error);
 
-public sealed record MutationError(object MutationCommand, Exception Exception)
-  : FluxError($"{FluxLogs.Flux_Mutation_ErrorMsg}: {Exception.Message}.", FluxLogs.Flux_Mutation_Error);
+public sealed record MutationError(string MutationCommandType, Exception Exception)
+	: FluxError($"{FluxLogs.Flux_Mutation_ErrorMsg} {MutationCommandType}: {Exception.Message}.", FluxLogs.Flux_Mutation_Error);
+
+public sealed record MappingError(string ViewModelType, Exception Exception)
+	: FluxError($"{FluxLogs.Flux_Mapping_ErrorMsg} {ViewModelType}: {Exception.Message}.", FluxLogs.Flux_Mutation_Error);
 
 public sealed record UnhandledFluxError(Exception Exception)
-    : FluxError($"{FluxLogs.Flux_Unhandled_ErrorMsg}: {Exception.Message}.", FluxLogs.Flux_Unhandled_Error);
+	: FluxError($"{FluxLogs.Flux_Unhandled_ErrorMsg}: {Exception.Message}.", FluxLogs.Flux_Unhandled_Error);
--- src/Flux/Carlton.Core.Flux/Logging/FluxLogs.cs
+++ src/Flux/Carlton.Core.Flux/Logging/FluxLogs.cs
@@ -5,135 +5,137 @@ namespace Carlton.Core.Flux.Logging;
 
 public static class FluxLogs
 {
-    //Logging Scopes
-    public const string FluxComponentInitialization = "FluxComponentInitialization:{@FluxComponentInitialization}";
-    public const string IsFluxChildRequest = "IsFluxChildRequest:{@IsFluxChildRequest}";
-    public const string FluxParentRequestId = "FluxParentRequestId:{@FluxParentRequestId}";
-    public const string FluxStateEvent = "FluxStateEvent:{@FluxStateEvent}";
-    public const string FluxAction = "FluxAction:{@FluxAction}";
-    public const string FluxRequestId = "FluxRequestId:{@FluxRequestId}";
-    public const string MutationCommand = "MutationCommand";
-    public const string ViewModelQuery = "ViewModelQuery";
-    public const string FluxRequestContext = "FluxRequestContext:{@FluxRequestContext}";
-    public const string ViewModelType = "ViewModelType:{ViewModelType}";
-    public const string MutationCommandType = "MutationCommandType:{MutationCommandType}";
-    public const string EventIdScope = "EventId:{@EventId}";
-    public const string Error = "Error:{@Error}";
-    public const string RequestErrored = "RequestErrored:{@RequestErrored}";
-    public const string JsModule = "JsModule:{@JsModule}";
-    public const string JsFunction = "JsFunction:{@JsFunction}";
-    public const string JsParameters = "JsParameters:{@JsParameters}";
+	//Logging Scopes
+	public const string FluxComponentInitialization = "FluxComponentInitialization:{@FluxComponentInitialization}";
+	public const string IsFluxChildRequest = "IsFluxChildRequest:{@IsFluxChildRequest}";
+	public const string FluxParentRequestId = "FluxParentRequestId:{@FluxParentRequestId}";
+	public const string FluxStateEvent = "FluxStateEvent:{@FluxStateEvent}";
+	public const string FluxAction = "FluxAction:{@FluxAction}";
+	public const string FluxRequestId = "FluxRequestId:{@FluxRequestId}";
+	public const string MutationCommand = "MutationCommand";
+	public const string ViewModelQuery = "ViewModelQuery";
+	public const string FluxRequestContext = "FluxRequestContext:{@FluxRequestContext}";
+	public const string ViewModelType = "ViewModelType:{ViewModelType}";
+	public const string MutationCommandType = "MutationCommandType:{MutationCommandType}";
+	public const string EventIdScope = "EventId:{@EventId}";
+	public const string Error = "Error:{@Error}";
+	public const string RequestErrored = "RequestErrored:{@RequestErrored}";
+	public const string JsModule = "JsModule:{@JsModule}";
+	public const string JsFunction = "JsFunction:{@JsFunction}";
+	public const string JsParameters = "JsParameters:{@JsParameters}";
 
-    //Flux Completed Events
-    public const int JsInterop_Completed = 1000;
-    public const int ViewModel_Completed = 2000;
-    public const int Mutation_Completed = 3000;
-   
-    //Flux Error Events
-    public const int Flux_Unhandled_Error = 100;
-    public const int Flux_Validation_Error = 101;
-    public const int Flux_HTTP_UnsupportedVerb_Error = 102;
-    public const int Flux_HTTP_Invalid_URL_Error = 103;
-    public const int Flux_HTTP_Invalid_URL_UnreplacedTokens_Error = 104;
-    public const int Flux_HTTP_FailedRequest_Error = 105;
-    public const int Flux_HTTP_Error = 106;
-    public const int Flux_JSON_Error = 107;
-    public const int Flux_MutationNotRegistered_Error = 108;
-    public const int Flux_Mutation_Error = 109;
-    public const int Flux_JsInterop_Error = 110;
+	//Flux Completed Events
+	public const int JsInterop_Completed = 1000;
+	public const int ViewModel_Completed = 2000;
+	public const int Mutation_Completed = 3000;
 
-    //Flux Error Messages
-    public const string Flux_Unhandled_ErrorMsg = "An unhandled exception occurred during a flux operation";
-    public const string Flux_Validation_ErrorMsg = "An error occurred while validating ViewModel of type";
-    public const string Flux_HTTP_UnsupportedVerb_ErrorMsg = "{0} is not a currently supported HTTP verb for flux server refresh operations.";
-    public const string Flux_HTTP_Invalid_URL_ErrorMsg = "An error occurred while constructing the remote server endpoint, {0} is not a valid URL.";
-    public const string Flux_HTTP_Invalid_URL_UnreplacedTokens_ErrorMsg = "The HTTP refresh endpoint is invalid, following URL parameters were not replaced";
-    public const string Flux_HTTP_FailedRequest_ErrorMsg = "The HTTP {0} request to {1} failed with status code {2} ({3}).";
-    public const string Flux_HTTP_ErrorMsg = "An exception occurred while sending an HTTP request";
-    public const string Flux_JSON_ErrorMsg = "An exception occurred while serializing or deserializing a JSON payload";
-    public const string Flux_MutationNotRegistered_ErrorMsg = "A mutation of type {0} has not been registered";
-    public const string Flux_Mutation_ErrorMsg = "A exception occurred while processing mutation of type {0}";
-    public const string Flux_JSInterop_ErrorMsg = "An error occurred during the JSInterop for a ViewModel of type";
+	//Flux Error Events
+	public const int Flux_Unhandled_Error = 100;
+	public const int Flux_Validation_Error = 101;
+	public const int Flux_HTTP_UnsupportedVerb_Error = 102;
+	public const int Flux_HTTP_Invalid_URL_Error = 103;
+	public const int Flux_HTTP_Invalid_URL_UnreplacedTokens_Error = 104;
+	public const int Flux_HTTP_FailedRequest_Error = 105;
+	public const int Flux_HTTP_Error = 106;
+	public const int Flux_JSON_Error = 107;
+	public const int Flux_MutationNotRegistered_Error = 108;
+	public const int Flux_Mutation_Error = 109;
+	public const int Flux_Mapping_Error = 110;
+	public const int Flux_JsInterop_Error = 111;
+
+	//Flux Error Messages
+	public const string Flux_Unhandled_ErrorMsg = "An unhandled exception occurred during a flux operation";
+	public const string Flux_Validation_ErrorMsg = "An error occurred while validating ViewModel of type";
+	public const string Flux_HTTP_UnsupportedVerb_ErrorMsg = "{0} is not a currently supported HTTP verb for flux server refresh operations.";
+	public const string Flux_HTTP_Invalid_URL_ErrorMsg = "An error occurred while constructing the remote server endpoint, {0} is not a valid URL.";
+	public const string Flux_HTTP_Invalid_URL_UnreplacedTokens_ErrorMsg = "The HTTP refresh endpoint is invalid, following URL parameters were not replaced";
+	public const string Flux_HTTP_FailedRequest_ErrorMsg = "The HTTP {0} request to {1} failed with status code {2} ({3}).";
+	public const string Flux_HTTP_ErrorMsg = "An exception occurred while sending an HTTP request";
+	public const string Flux_JSON_ErrorMsg = "An exception occurred while serializing or deserializing a JSON payload";
+	public const string Flux_MutationNotRegistered_ErrorMsg = "mutation of type {0} has not been registered";
+	public const string Flux_Mutation_ErrorMsg = "An exception occurred while processing mutation of type";
+	public const string Flux_Mapping_ErrorMsg = "An exception occurred while mapping to ViewModel of type";
+	public const string Flux_JSInterop_ErrorMsg = "An error occurred during the JSInterop for a ViewModel of type";
 
 
-    //Friendly Error
-    public const string FriendlyErrorMsg = "Oops! We are sorry an error has occurred. Please try again.";
+	//Friendly Error
+	public const string FriendlyErrorMsg = "Oops! We are sorry an error has occurred. Please try again.";
 
-    public static IDisposable BeginJsInteropLoggingScopes(this ILogger logger, string jsModule, string jsFunction, object[] jsParameters)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(JsModule, jsModule),
-            logger.BeginScope(JsFunction, jsFunction),
-            logger.BeginScope(JsParameters, jsParameters)
-        );
-    }
+	public static IDisposable BeginJsInteropLoggingScopes(this ILogger logger, string jsModule, string jsFunction, object[] jsParameters)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(JsModule, jsModule),
+			logger.BeginScope(JsFunction, jsFunction),
+			logger.BeginScope(JsParameters, jsParameters)
+		);
+	}
 
-    public static IDisposable BeginRequestErrorLoggingScopes(this ILogger logger, FluxError error)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(EventIdScope, error.EventId),
-            logger.BeginScope(Error, error),
-            logger.BeginScope(RequestErrored, true)
-        );
-    }
+	public static IDisposable BeginRequestErrorLoggingScopes(this ILogger logger, FluxError error)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(EventIdScope, error.EventId),
+			logger.BeginScope(Error, error),
+			logger.BeginScope(RequestErrored, true)
+		);
+	}
 
-    public static IDisposable BeginRequestErrorLoggingScopes(this ILogger logger, int eventId)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(EventIdScope, eventId),
-            logger.BeginScope(RequestErrored, true)
-        );
-    }
+	public static IDisposable BeginRequestErrorLoggingScopes(this ILogger logger, int eventId)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(EventIdScope, eventId),
+			logger.BeginScope(RequestErrored, true)
+		);
+	}
 
-    public static IDisposable BeginFluxComponentChildRequestLoggingScopes(this ILogger logger, Guid requestId)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(IsFluxChildRequest, true),
-            logger.BeginScope(FluxParentRequestId, requestId)
-        );
-    }
+	public static IDisposable BeginFluxComponentChildRequestLoggingScopes(this ILogger logger, Guid requestId)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(IsFluxChildRequest, true),
+			logger.BeginScope(FluxParentRequestId, requestId)
+		);
+	}
 
-    public static IDisposable BeginFluxComponentEventReceivedLoggingScopes(this ILogger logger, string stateEvent)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(FluxStateEvent, stateEvent)
-        );
-    }
+	public static IDisposable BeginFluxComponentEventReceivedLoggingScopes(this ILogger logger, string stateEvent)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(FluxStateEvent, stateEvent)
+		);
+	}
 
-    public static IDisposable BeginViewModelInitializationLoggingScopes(this ILogger logger)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(FluxComponentInitialization, true)
-        );
-    }
+	public static IDisposable BeginViewModelInitializationLoggingScopes(this ILogger logger)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(FluxComponentInitialization, true)
+		);
+	}
 
-    public static IDisposable BeginViewModelRequestLoggingScopes<TViewModel>(this ILogger logger, ViewModelQueryContext<TViewModel> context)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(FluxAction, ViewModelQuery),
-            logger.BeginScope(ViewModelType, context.FluxOperationTypeName),
-            logger.BeginScope(FluxRequestId, context.RequestId),
-            logger.BeginScope(FluxRequestContext, context)
-        );
-    }
+	public static IDisposable BeginViewModelRequestLoggingScopes<TViewModel>(this ILogger logger, ViewModelQueryContext<TViewModel> context)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(FluxAction, ViewModelQuery),
+			logger.BeginScope(ViewModelType, context.FluxOperationTypeName),
+			logger.BeginScope(FluxRequestId, context.RequestId),
+			logger.BeginScope(FluxRequestContext, context)
+		);
+	}
 
-    public static IDisposable BeginMutationCommandRequestLoggingScopes<TCommand>(this ILogger logger, MutationCommandContext<TCommand> context)
-    {
-        return new CompositeDisposable
-        (
-            logger.BeginScope(FluxAction, MutationCommand),
-            logger.BeginScope(MutationCommandType, context.FluxOperationTypeName),
-            logger.BeginScope(FluxRequestId, context.RequestId),
-            logger.BeginScope(FluxRequestContext, context)
-        );
-    }
+	public static IDisposable BeginMutationCommandRequestLoggingScopes<TCommand>(this ILogger logger, MutationCommandContext<TCommand> context)
+	{
+		return new CompositeDisposable
+		(
+			logger.BeginScope(FluxAction, MutationCommand),
+			logger.BeginScope(MutationCommandType, context.FluxOperationTypeName),
+			logger.BeginScope(FluxRequestId, context.RequestId),
+			logger.BeginScope(FluxRequestContext, context)
+		);
+	}
 }
 
 
