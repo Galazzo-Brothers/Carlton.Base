a57a473bf98d3e27dc248dfdefaf0aacd5cb82cf
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 12:43:28 -0500

    viewModelQueryDispatcher now catches and returns potential mapping errors

--- src/Flux/Carlton.Core.Flux/Dispatchers/ViewModels/ViewModelQueryDispatcher.cs
+++ src/Flux/Carlton.Core.Flux/Dispatchers/ViewModels/ViewModelQueryDispatcher.cs
@@ -2,21 +2,23 @@
 
 public class ViewModelQueryDispatcher<TState>(IServiceProvider serviceProvider) : IViewModelQueryDispatcher<TState>
 {
-    private readonly IServiceProvider _serviceProvider = serviceProvider;
+	private readonly IServiceProvider _serviceProvider = serviceProvider;
 
-    public async Task<Result<TViewModel, FluxError>> Dispatch<TViewModel>(object sender, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
-    {
-        var handler = _serviceProvider.GetRequiredService<IViewModelQueryHandler<TState>>();
-        return await handler.Handle(context, cancellationToken);
-    }
+	public async Task<Result<TViewModel, FluxError>> Dispatch<TViewModel>(object sender, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
+	{
+		var handler = _serviceProvider.GetRequiredService<IViewModelQueryHandler<TState>>();
+		return await handler.Handle(context, cancellationToken);
+	}
 }
 
 
 public class ViewModelQueryHandler<TState>(IFluxState<TState> _state, IViewModelMapper<TState> _mapper) : IViewModelQueryHandler<TState>
 {
-    public Task<Result<TViewModel, FluxError>> Handle<TViewModel>(ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
-    {
-        var vm = _mapper.Map<TViewModel>(_state.CurrentState);
-        return Task.FromResult((Result<TViewModel, FluxError>)vm);
-    }
+	public Task<Result<TViewModel, FluxError>> Handle<TViewModel>(ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
+	{
+		var vm = ResultExtensions.TryResult<TViewModel, FluxError, Exception>(
+			() => _mapper.Map<TViewModel>(_state.CurrentState),
+			ex => new MappingError(context.FluxOperationTypeName, ex));
+		return Task.FromResult(vm);
+	}
 }
