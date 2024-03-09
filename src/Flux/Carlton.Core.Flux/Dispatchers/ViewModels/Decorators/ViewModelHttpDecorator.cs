cd58f49d3c71755733caae812b43e75a63412f73
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 12:43:28 -0500

    viewModelQueryDispatcher now catches and returns potential mapping errors

--- src/Flux/Carlton.Core.Flux/Dispatchers/ViewModels/Decorators/ViewModelHttpDecorator.cs
+++ src/Flux/Carlton.Core.Flux/Dispatchers/ViewModels/Decorators/ViewModelHttpDecorator.cs
@@ -4,104 +4,104 @@ using Carlton.Core.Flux.Handlers.Base;
 namespace Carlton.Core.Flux.Dispatchers.ViewModels.Decorators;
 
 public class ViewModelHttpDecorator<TState>(
-    IViewModelQueryDispatcher<TState> _decorated,
-    HttpClient _client,
-    IMutableFluxState<TState> _state)
-    : BaseHttpDecorator<TState>(_client), IViewModelQueryDispatcher<TState>
+	IViewModelQueryDispatcher<TState> _decorated,
+	HttpClient _client,
+	IMutableFluxState<TState> _state)
+	: BaseHttpDecorator<TState>(_client), IViewModelQueryDispatcher<TState>
 {
-    public async Task<Result<TViewModel, FluxError>> Dispatch<TViewModel>(object sender, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
-    {
-        //Get FluxServerCommunicationAttribute Attribute
-        var attributes = sender.GetType().GetCustomAttributes();
-        var fluxServerCommunicationAttribute = attributes.OfType<FluxServerCommunicationAttribute>().FirstOrDefault();
-        var requiresRefresh = GetRefreshPolicy(fluxServerCommunicationAttribute?.ServerCommunicationPolicy);
+	public async Task<Result<TViewModel, FluxError>> Dispatch<TViewModel>(object sender, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
+	{
+		//Get FluxServerCommunicationAttribute Attribute
+		var attributes = sender.GetType().GetCustomAttributes();
+		var fluxServerCommunicationAttribute = attributes.OfType<FluxServerCommunicationAttribute>().FirstOrDefault();
+		var requiresRefresh = GetRefreshPolicy(fluxServerCommunicationAttribute?.ServerCommunicationPolicy);
 
-        if (requiresRefresh)
-        {
-            //Update the context for logging and auditing
-            context.MarkAsRequiresHttpRefresh();
+		if (requiresRefresh)
+		{
+			//Update the context for logging and auditing
+			context.MarkAsRequiresHttpRefresh();
+				
+			//Find FluxServerCommunicationParameterAttributes
+			var parameterAttributes = sender.GetType().GetProperties().SelectMany(p => p.GetCustomAttributes<FluxServerCommunicationParameterAttribute>());
 
-            //Find FluxServerCommunicationParameterAttributes
-            var parameterAttributes = sender.GetType().GetProperties().SelectMany(p => p.GetCustomAttributes<FluxServerCommunicationParameterAttribute>());
+			//Construct Http Refresh URL
+			var serverUrlResult = GetServerUrl(fluxServerCommunicationAttribute, parameterAttributes, sender);
 
-            //Construct Http Refresh URL
-            var serverUrlResult = GetServerUrl(fluxServerCommunicationAttribute, parameterAttributes, sender);
+			//Get ViewModel from server
+			var vmResult = await serverUrlResult.Match
+				(
+					async serverUrl =>
+					{
+						//Get ViewModel from server
+						var vmResult = await GetHttpViewModel(serverUrl, context, cancellationToken);
 
-            //Get ViewModel from server
-            var vmResult = await serverUrlResult.Match
-                (
-                    async serverUrl =>
-                    {
-                        //Get ViewModel from server
-                        var vmResult = await GetHttpViewModel(serverUrl, context, cancellationToken);
+						//Update the StateStore and pickup any mutation errors
+						vmResult = await ApplyViewModelStateMutation(vmResult, context);
 
-                        //Update the StateStore and pickup any mutation errors
-                        vmResult = await ApplyViewModelStateMutation(vmResult, context);
+						//Return result
+						return vmResult;
+					},
+					err => err.ToResultTask<TViewModel, FluxError>()
+				);
 
-                        //Return result
-                        return vmResult;
-                    },
-                    err => err.ToResultTask<TViewModel, FluxError>()
-                );
+			//If http refresh failed return error
+			if (!vmResult.IsSuccess)
+				return vmResult;
+		}
 
-            //If http refresh failed return error
-            if (!vmResult.IsSuccess)
-                return vmResult;
-        }
+		//Continue with Dispatch
+		return await _decorated.Dispatch(sender, context, cancellationToken);
+	}
 
-        //Continue with Dispatch
-        return await _decorated.Dispatch(sender, context, cancellationToken);
-    }
+	private async Task<Result<TViewModel, FluxError>> GetHttpViewModel<TViewModel>(string serverUrl, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
+	{
+		try
+		{
+			//Get ViewModel from server
+			var response = await Client.GetAsync(serverUrl, cancellationToken);
 
-    private async Task<Result<TViewModel, FluxError>> GetHttpViewModel<TViewModel>(string serverUrl, ViewModelQueryContext<TViewModel> context, CancellationToken cancellationToken)
-    {
-        try
-        {
-            //Get ViewModel from server
-            var response = await Client.GetAsync(serverUrl, cancellationToken);
-            
-            //Return error value if http call unsuccessful
-            if (!response.IsSuccessStatusCode)
-                return HttpRequestFailedError(response);
+			//Return error value if http call unsuccessful
+			if (!response.IsSuccessStatusCode)
+				return HttpRequestFailedError(response);
 
-            //Deserialize the content to the specified type
-            var viewModel = await response.Content.ReadFromJsonAsync<TViewModel>(cancellationToken: cancellationToken);
+			//Deserialize the content to the specified type
+			var viewModel = await response.Content.ReadFromJsonAsync<TViewModel>(cancellationToken: cancellationToken);
 
-            //Update the context and return the ViewModel
-            context.MarkAsHttpCallMade(serverUrl, System.Net.HttpStatusCode.OK, viewModel);
-            return viewModel;
-        }
-        catch (HttpRequestException ex)
-        {
-            return HttpError(ex);
-        }
-        catch (JsonException ex)
-        {
-            return JsonError(ex);
-        }
-        catch (NotSupportedException ex) when (ex.Message.Contains("Serialization and deserialization"))
-        {
-            return JsonError(ex);
-        }
-    }
+			//Update the context and return the ViewModel
+			context.MarkAsHttpCallMade(serverUrl, System.Net.HttpStatusCode.OK, viewModel);
+			return viewModel;
+		}
+		catch (HttpRequestException ex)
+		{
+			return HttpError(ex);
+		}
+		catch (JsonException ex)
+		{
+			return JsonError(ex);
+		}
+		catch (NotSupportedException ex) when (ex.Message.Contains("Serialization and deserialization"))
+		{
+			return JsonError(ex);
+		}
+	}
 
-    private async Task<Result<TViewModel, FluxError>> ApplyViewModelStateMutation<TViewModel>(Result<TViewModel, FluxError> vmResult, ViewModelQueryContext<TViewModel> context)
-    {
-        return await vmResult.Match
-        (
-            async vm =>
-            {
-                //Apply State Mutation
-                var result = await _state.ApplyMutationCommand(vm);
+	private async Task<Result<TViewModel, FluxError>> ApplyViewModelStateMutation<TViewModel>(Result<TViewModel, FluxError> vmResult, ViewModelQueryContext<TViewModel> context)
+	{
+		return await vmResult.Match
+		(
+			async vm =>
+			{
+				//Apply State Mutation
+				var result = await _state.ApplyMutationCommand(vm);
 
-                //Update Context
-                if(result.IsSuccess)
-                    context.MarkAsStateModifiedByHttpRefresh();
+				//Update Context
+				if (result.IsSuccess)
+					context.MarkAsStateModifiedByHttpRefresh();
 
-                //Return Result
-                return result;
-            },
-            err => err.ToResultTask<TViewModel, FluxError>()
-        );
-    }
+				//Return Result
+				return result;
+			},
+			err => err.ToResultTask<TViewModel, FluxError>()
+		);
+	}
 }
