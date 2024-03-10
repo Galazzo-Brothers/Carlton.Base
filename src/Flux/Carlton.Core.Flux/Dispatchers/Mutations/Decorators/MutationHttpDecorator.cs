3d2bf0bb4a4f25f01594ecaf76f4c3a302163a67
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-09 23:49:15 -0500

    modified MutationHttpDecorator to only support same payload from server added new uint tests

--- src/Flux/Carlton.Core.Flux/Dispatchers/Mutations/Decorators/MutationHttpDecorator.cs
+++ src/Flux/Carlton.Core.Flux/Dispatchers/Mutations/Decorators/MutationHttpDecorator.cs
@@ -4,142 +4,106 @@ using System.Net.Http.Json;
 namespace Carlton.Core.Flux.Dispatchers.Mutations.Decorators;
 
 public class MutationHttpDecorator<TState>(IMutationCommandDispatcher<TState> _decorated, HttpClient _client)
-    : BaseHttpDecorator<TState>(_client), IMutationCommandDispatcher<TState>
+	: BaseHttpDecorator<TState>(_client), IMutationCommandDispatcher<TState>
 {
-    public async Task<Result<MutationCommandResult, FluxError>> Dispatch<TCommand>(object sender, MutationCommandContext<TCommand> context, CancellationToken cancellationToken)
-    {
-        var attributes = sender.GetType().GetCustomAttributes();
-        var fluxServerCommunicationAttribute = attributes.OfType<FluxServerCommunicationAttribute>().FirstOrDefault();
-        var requiresRefresh = GetRefreshPolicy(fluxServerCommunicationAttribute.ServerCommunicationPolicy);
+	public async Task<Result<MutationCommandResult, FluxError>> Dispatch<TCommand>(object sender, MutationCommandContext<TCommand> context, CancellationToken cancellationToken)
+	{
+		var attributes = sender.GetType().GetCustomAttributes();
+		var fluxServerAttribute = attributes.OfType<FluxServerCommunicationAttribute>().FirstOrDefault();
+		var requiresRefresh = GetRefreshPolicy(fluxServerAttribute?.ServerCommunicationPolicy);
 
-        //Continue on if no refresh required
-        if (requiresRefresh)
-        {
-            //Update the context for logging and auditing
-            context.MarkAsRequiresHttpRefresh();
+		//Continue on if no refresh required
+		if (requiresRefresh)
+		{
+			//Update the context for logging and auditing
+			context.MarkAsRequiresHttpRefresh();
 
-            //Construct Http Refresh URL
-            var urlParameterAttributes = attributes.OfType<FluxServerCommunicationParameterAttribute>() ?? new List<FluxServerCommunicationParameterAttribute>();
-            var serverUrlResult = GetServerUrl(fluxServerCommunicationAttribute, urlParameterAttributes, sender);
+			//Construct Http Refresh URL
+			var urlParameterAttributes = attributes.OfType<FluxServerCommunicationParameterAttribute>() ?? new List<FluxServerCommunicationParameterAttribute>();
+			var serverUrlResult = GetServerUrl(fluxServerAttribute, urlParameterAttributes, sender);
 
-            //Send Request
-            var response = await SendRequest(fluxServerCommunicationAttribute.HttpVerb, serverUrlResult, context, cancellationToken);
+			//Send Request
+			var response = await SendRequest(fluxServerAttribute.HttpVerb, serverUrlResult, context, fluxServerAttribute.UpdateWithResponseBody, cancellationToken);
 
-            //Update context with response
-            await UpdateCommandWithServerResponse(response, context, cancellationToken);
-        }
+			//If http request or parse failed, return error
+			if (!response.IsSuccess)
+				return response;
+		}
 
-        return await _decorated.Dispatch(sender, context, cancellationToken); ;
-    }
+		return await _decorated.Dispatch(sender, context, cancellationToken);
+	}
 
-    protected async Task<Result<HttpResponseMessage, FluxError>> SendRequest<TCommand>(
-        HttpVerb httpVerb,
-        Result<string, FluxError> serverUrlResult,
-        MutationCommandContext<TCommand> context,
-        CancellationToken cancellationToken)
-    {
-        return await serverUrlResult.Match
-        (
-            async serverUrl =>
-            {
-                try
-                {
-                    Result<HttpResponseMessage, FluxError> response = httpVerb switch
-                    {
-                        HttpVerb.POST => await Client.PostAsJsonAsync(serverUrl, context, cancellationToken),
-                        HttpVerb.PUT => await Client.PutAsJsonAsync(serverUrl, context, cancellationToken),
-                        HttpVerb.PATCH => await Client.PatchAsJsonAsync(serverUrl, context, cancellationToken),
-                        HttpVerb.DELETE => await Client.DeleteAsync(serverUrl, cancellationToken),
-                        _ => UnsupportedHttpVerbError(httpVerb).ToResult<HttpResponseMessage, FluxError>(),
-                    };
+	protected async Task<Result<MutationCommandResult, FluxError>> SendRequest<TCommand>(
+		HttpVerb httpVerb,
+		Result<string, FluxError> serverUrlResult,
+		MutationCommandContext<TCommand> context,
+		bool updateWithResponseBody,
+		CancellationToken cancellationToken)
+	{
+		return await serverUrlResult.Match
+		(
+			async serverUrl =>
+			{
+				try
+				{
+					//Execute Http Request
+					Result<HttpResponseMessage, FluxError> response = httpVerb switch
+					{
+						HttpVerb.POST => await Client.PostAsJsonAsync(serverUrl, context, cancellationToken),
+						HttpVerb.PUT => await Client.PutAsJsonAsync(serverUrl, context, cancellationToken),
+						HttpVerb.PATCH => await Client.PatchAsJsonAsync(serverUrl, context, cancellationToken),
+						HttpVerb.DELETE => await Client.DeleteAsync(serverUrl, cancellationToken),
+						_ => UnsupportedHttpVerbError(httpVerb).ToResult<HttpResponseMessage, FluxError>(),
+					};
 
-                    context.MarkAsHttpCallMade(serverUrl, System.Net.HttpStatusCode.OK, response);
+					//Parse Http Response
+					return await response.Match
+					(
+						async serverResponse => await HandleSuccess(context, updateWithResponseBody, serverUrl, serverResponse, cancellationToken),
+						err => err.ToResultTask<MutationCommandResult, FluxError>()
+					);
+				}
+				catch (HttpRequestException ex)
+				{
+					return HttpError(ex);
+				}
+				catch (JsonException ex)
+				{
+					return JsonError(ex);
+				}
+				catch (NotSupportedException ex) when (ex.Message.Contains("Serialization and deserialization"))
+				{
+					return JsonError(ex);
+				}
+			},
+			err => err.ToResultTask<MutationCommandResult, FluxError>()
+		);
+	}
 
-                    return response;
-                }
-                catch (HttpRequestException ex)
-                {
-                    return HttpError(ex);
-                }
-                catch (JsonException ex)
-                {
-                    return JsonError(ex);
-                }
-                catch (NotSupportedException ex) when (ex.Message.Contains("Serialization and deserialization"))
-                {
-                    return JsonError(ex);
-                }
-            },
-            err => err.ToResultTask<HttpResponseMessage, FluxError>()
-        );
-    }
+	private static async Task<Result<MutationCommandResult, FluxError>> HandleSuccess<TCommand>(
+		MutationCommandContext<TCommand> context,
+		bool updateWithResponseBody,
+		string serverUrl,
+		HttpResponseMessage serverResponse,
+		CancellationToken cancellationToken)
+	{
+		//Update Context with Http Request
+		context.MarkAsHttpCallMade(serverUrl, System.Net.HttpStatusCode.OK, serverResponse);
 
-    private static async Task<Result<bool, FluxError>> UpdateCommandWithServerResponse<TCommand>(
-        Result<HttpResponseMessage, FluxError> responseResult,
-        MutationCommandContext<TCommand> context,
-        CancellationToken cancellationToken)
-    {
-        return await responseResult.Match
-        (
-            async response =>
-            {
-                try
-                {
-                    //return error
-                    if (!response.IsSuccessStatusCode)
-                        return HttpRequestFailedError(response);
+		//If http request failed, return error
+		if (!serverResponse.IsSuccessStatusCode)
+			return HttpRequestFailedError(serverResponse);
 
-                    //parse json
-                    var json = await response.Content.ReadAsStringAsync(cancellationToken);
+		//Check if command should be replaced with response body
+		if (updateWithResponseBody)
+		{
+			var serverCommand = await serverResponse.Content.ReadFromJsonAsync<TCommand>(cancellationToken);
+			context.ReplaceCommandWithResponseBody(serverCommand);
+		}
 
-                    //Find HttpResponseType attribute
-                    var serverResponseTypeAttribute = context.MutationCommand.GetType().GetCustomAttribute(typeof(HttpResponseTypeAttribute<>));
-
-                    //Exit if response type attribute not present
-                    if (serverResponseTypeAttribute == null)
-                        return true;
-
-                    //Find HttpResponseProperty attributes
-                    var replacementProperties = context.MutationCommand.GetType().GetProperties().Where(Predicate);
-
-                    //Exit if response property attributes not present
-                    if (!replacementProperties.Any())
-                        return true;
-
-                    //Parse the server json response
-                    var serverResponseType = serverResponseTypeAttribute.GetType().GetGenericArguments()[0];
-                    var parsedResponse = JsonSerializer.Deserialize(json, serverResponseType, new JsonSerializerOptions(JsonSerializerDefaults.Web));
-
-                    //iterate through properties
-                    foreach (var prop in replacementProperties)
-                    {
-                        //Get the property attribute
-                        var attribute = prop.GetCustomAttribute<HttpResponsePropertyAttribute>();
-
-                        //Pull the property value from the server response
-                        var serverResponseValue = parsedResponse.GetType().GetProperty(attribute.ResponsePropertyName).GetValue(parsedResponse);
-
-                        //Update the command with the server response value
-                        prop.SetValue(context.MutationCommand, serverResponseValue);
-                    }
-
-                    return true;
-                }
-                catch (JsonException ex)
-                {
-                    return JsonError(ex);
-                }
-                catch (NotSupportedException ex) when (ex.Message.Contains("Serialization and deserialization"))
-                {
-                    return JsonError(ex);
-                }
-            },
-            err => err.ToResultTask<bool, FluxError>()
-        );
-    }
-
-    //Static inline helper predicate
-    static bool Predicate(PropertyInfo prop) => Attribute.IsDefined(prop, typeof(HttpResponsePropertyAttribute));
+		return new MutationCommandResult();
+	}
 }
+
 
 
