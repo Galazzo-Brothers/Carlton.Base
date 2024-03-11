dde90faabffd7d132c28357b3687ff511d61e2ef
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-10 21:03:29 -0400

    created one singel FluxException used to pass the Error up through to the Erroboundary

---
+++ src/Flux/Carlton.Core.Flux/Exceptions/FluxException.cs
@@ -0,0 +1,8 @@
+﻿using Carlton.Core.Flux.Dispatchers;
+namespace Carlton.Core.Flux.Exceptions;
+
+internal class FluxException(FluxError error, FluxOperation fluxOperation) : Exception
+{
+	public FluxError Error { get; } = error;
+	public FluxOperation FuxOperation { get; } = fluxOperation;
+}
--- src/Flux/Carlton.Core.Flux/Exceptions/ViewModelQueryException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Carlton.Core.Flux.Exceptions;
-
-internal class ViewModelQueryException(FluxError error) : Exception
-{
-    public FluxError Error { get; } = error;
-}
