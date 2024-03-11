def6ae2f822bb5303d8b28c0d3eed9424a7a1f1c
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-10 21:03:29 -0400

    created one singel FluxException used to pass the Error up through to the Erroboundary

--- src/Flux/Carlton.Core.Flux/Exceptions/MutationCommandException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Carlton.Core.Flux.Exceptions;
-
-internal class MutationCommandException(FluxError error) : Exception
-{
-    public FluxError Error { get; } = error;
-}
--- src/Flux/Carlton.Core.Flux/Extensions/ResultExtensions.cs
+++ src/Flux/Carlton.Core.Flux/Extensions/ResultExtensions.cs
@@ -1,23 +1,24 @@
-﻿using Carlton.Core.Flux.Exceptions;
+﻿using Carlton.Core.Flux.Dispatchers;
+using Carlton.Core.Flux.Exceptions;
 namespace Carlton.Core.Flux.Extensions;
 
 internal static class ResultExtensions
 {
-    internal static TViewModel GetViewModelResultOrThrow<TViewModel>(this Result<TViewModel, FluxError> result)
-    {
-        return result.Match
-               (
-                   vm => vm,
-                   err => throw new ViewModelQueryException(err)
-               );
-    }
+	internal static TViewModel GetViewModelResultOrThrow<TViewModel>(this Result<TViewModel, FluxError> result)
+	{
+		return result.Match
+			   (
+				   vm => vm,
+				   err => throw new FluxException(err, FluxOperation.ViewModelQuery)
+			   );
+	}
 
-    internal static MutationCommandResult GetMutationResultOrThrow(this Result<MutationCommandResult, FluxError> result)
-    {
-        return result.Match
-               (
-                   r => r,
-                   err => throw new MutationCommandException(err)
-               );
-    }
+	internal static MutationCommandResult GetMutationResultOrThrow(this Result<MutationCommandResult, FluxError> result)
+	{
+		return result.Match
+			   (
+				   r => r,
+				   err => throw new FluxException(err, FluxOperation.CommandMutation)
+			   );
+	}
 }
