94449313ad69b222fc275c9f0acbccee3e3a6f6c
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-12 16:58:55 -0400

    flux debug hack in order for it to build for now

--- src/Flux/Carlton.Core.Flux.Debug/Models/Common/TraceLogMessage.cs
+++ src/Flux/Carlton.Core.Flux.Debug/Models/Common/TraceLogMessage.cs
@@ -1,38 +1,37 @@
-﻿using Carlton.Core.Flux.Dispatchers;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 namespace Carlton.Core.Flux.Debug.Models.Common;
 
 public record TraceLogMessage
 {
-    public required DateTime Timestamp { get; init; }
-    public required FluxActions FluxAction { get; init; }
-    public required EventId EventId { get; init; }
-    public required string TypeDisplayName { get; init; }
-    public required bool RequestSucceeded { get; init; }
-    public required BaseRequestContext RequestContext { get; init; }
+	public required DateTime Timestamp { get; init; }
+	public required FluxActions FluxAction { get; init; }
+	public required EventId EventId { get; init; }
+	public required string TypeDisplayName { get; init; }
+	public required bool RequestSucceeded { get; init; }
+	public required object RequestContext { get; init; }
 }
 
 public record TraceLogMessageGroup
 {
-    public required TraceLogMessage ParentEntry { get; set; }
-    public IEnumerable<TraceLogMessage> ChildEntries { get; set; } = new List<TraceLogMessage>();
+	public required TraceLogMessage ParentEntry { get; set; }
+	public IEnumerable<TraceLogMessage> ChildEntries { get; set; } = new List<TraceLogMessage>();
 
-    public IEnumerable<TraceLogMessage> FlattenedEntries
-    {
-        get
-        {
-            // Include the parent entry first, followed by child entries
-            yield return ParentEntry;
-            foreach (var childEntry in ChildEntries)
-            {
-                yield return childEntry;
-            }
-        }
-    }
+	public IEnumerable<TraceLogMessage> FlattenedEntries
+	{
+		get
+		{
+			// Include the parent entry first, followed by child entries
+			yield return ParentEntry;
+			foreach (var childEntry in ChildEntries)
+			{
+				yield return childEntry;
+			}
+		}
+	}
 }
 
 public enum FluxActions
 {
-    ViewModelQuery,
-    MutationCommand
+	ViewModelQuery,
+	MutationCommand
 }
