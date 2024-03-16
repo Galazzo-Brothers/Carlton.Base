78a2453e509f92ce0e931cd59a0dd3d1e43b4156
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-16 09:01:26 -0400

    restored mapping tests for labstate

--- src/Lab/Carlton.Core.Lab/GlobalUsings.cs
+++ src/Lab/Carlton.Core.Lab/GlobalUsings.cs
@@ -1,6 +1,5 @@
 ﻿global using System;
 global using System.Collections.Generic;
-global using System.ComponentModel.DataAnnotations;
 global using System.Linq;
 global using System.Threading.Tasks;
 global using Carlton.Core.Utilities.Extensions;
@@ -8,5 +7,4 @@ global using Carlton.Core.Flux.Contracts;
 global using Carlton.Core.Lab.Models.Common;
 global using Carlton.Core.Lab.Models.Commands;
 global using Carlton.Core.Lab.Models.ViewModels;
-global using ViewModels = Carlton.Core.Lab.Models.ViewModels;
 
