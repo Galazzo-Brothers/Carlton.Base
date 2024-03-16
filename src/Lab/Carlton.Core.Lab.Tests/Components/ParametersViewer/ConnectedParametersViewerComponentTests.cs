72ceb50781a348c8a1f563623e708625fb0e1274
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-15 21:13:23 -0400

    added parametersviewer component unit tests

---
+++ src/Lab/Carlton.Core.Lab.Tests/Components/ParametersViewer/ConnectedParametersViewerComponentTests.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.Json;
+using System.Web;
+using Carlton.Core.Lab.Components.ParameterViewer;
+namespace Carlton.Core.Lab.Test.Components.ParametersViewer;
+
+public class ConnectedParametersViewerComponentTests : TestContext
+{
+	[Theory, AutoData]
+	public void ConnectedParametersViewer_Markup_RendersCorrectly(
+		TestParameters expectedParameters)
+	{
+		//Arrange
+		var viewModel = new ParametersViewerViewModel { ComponentParameters = expectedParameters };
+		var expectedMarkup = BuildExpectedMarkup(viewModel.ComponentParameters);
+
+		//Act
+		var cut = RenderComponent<ConnectedParametersViewer>(parameters => parameters
+					.Add(p => p.ViewModel, viewModel));
+
+		//Assert
+		cut.MarkupMatches(expectedMarkup);
+	}
+
+
+	[Theory, AutoData]
+	public void ConnectedParameterViewerComponent_OnParametersChanged_RaisesComponentEvent(
+		TestParameters initialParameters,
+		TestParameters expectedParameters)
+	{
+		//Arrange
+		var eventFired = false;
+		var evetArgs = (object)null;
+		var viewModel = new ParametersViewerViewModel { ComponentParameters = initialParameters };
+		var command = new UpdateParametersCommand { Parameters = expectedParameters };
+		var cut = RenderComponent<ConnectedParametersViewer>(parameters => parameters
+				.Add(p => p.ViewModel, viewModel)
+				.Add(p => p.OnComponentEvent, args =>
+				{
+					eventFired = true;
+					evetArgs = args;
+				}));
+
+		var newJson = JsonSerializer.Serialize(expectedParameters, new JsonSerializerOptions { WriteIndented = true });
+		var txt = cut.Find("textarea");
+
+		//Act
+		txt.Change(newJson);
+
+		//Assert
+		eventFired.ShouldBeTrue();
+		evetArgs.ShouldBeOfType<UpdateParametersCommand>()
+			.Parameters.ShouldBe(expectedParameters);
+	}
+
+	private static string BuildExpectedMarkup(object obj)
+	{
+		var parameterString = HttpUtility.HtmlEncode(JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true }));
+
+		return @$"
+<div class=""parameters-viewer"">
+    <div class=""json-viewer-console"">
+        <div class=""console"">
+            <textarea rows = ""15"" class="""" value=""{parameterString}""></textarea>
+        </div>
+    </div>
+</div>";
+	}
+}
+
