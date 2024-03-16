5d14308cea2504d50997057211203cc6f75f61c3
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-16 15:20:57 -0400

    added lab layout tests

---
+++ src/Lab/Carlton.Core.Lab.Tests/Layout/LabLayoutComponentTests.cs
@@ -0,0 +1,25 @@
+﻿using Carlton.Core.Components.Layouts.FullScreen;
+using Carlton.Core.Foundation.Test;
+using Carlton.Core.Lab.Layouts;
+using Microsoft.Extensions.DependencyInjection;
+namespace Carlton.Core.Lab.Test.Layout;
+
+public class LabLayoutComponentTests : TestContext
+{
+	[Theory, AutoNSubstituteData]
+	public void LabLayout_Markup_RendersCorrectly(
+		string body,
+		IFullScreenState fullScreenState)
+	{
+		//Arrange
+		Services.AddSingleton(fullScreenState);
+		var expectedMarkup = $@"{body}";
+
+		//Act
+		var cut = RenderComponent<LabLayout>(parameters =>
+			parameters.Add(p => p.Body, body));
+
+		//Assert
+		cut.MarkupMatches(expectedMarkup);
+	}
+}
---
+++ src/Lab/Carlton.Core.Lab.Tests/Layout/LabTabsComponentTests.cs
@@ -0,0 +1,18 @@
+﻿using Carlton.Core.Lab.Layouts;
+namespace Carlton.Core.Lab.Test.Layout;
+
+public class LabTabsComponentTests : TestContext
+{
+	[Fact]
+	public void LabTabs_Markup_RendersCorrectly()
+	{
+		//Arrange
+		var expectedMarkup = $@"";
+
+		//Act
+		var cut = RenderComponent<LabTabs>();
+
+		//Assert
+		cut.MarkupMatches(expectedMarkup);
+	}
+}
