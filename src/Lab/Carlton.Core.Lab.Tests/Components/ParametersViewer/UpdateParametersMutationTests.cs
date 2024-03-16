30d9070ef5e5df9ea46282239a2d2c15a5ad5625
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-15 21:13:23 -0400

    added parametersviewer component unit tests

---
+++ src/Lab/Carlton.Core.Lab.Tests/Components/ParametersViewer/UpdateParametersMutationTests.cs
@@ -0,0 +1,23 @@
+﻿using Carlton.Core.Lab.Components.ParametersViewer;
+using Carlton.Core.Lab.Models.Common;
+namespace Carlton.Core.Lab.Test.Components.ParametersViewer;
+
+public class UpdateParametersMutationTests
+{
+	[Theory, AutoData]
+	public void UpdateParametersMutation_MutatesCorrectly(
+		IEnumerable<ComponentConfigurations> componentConfigurations,
+		UpdateParametersMutation sut,
+		TestParameters expectedParameters)
+	{
+		//Arrange
+		var state = new LabState(componentConfigurations);
+		var command = new UpdateParametersCommand { Parameters = expectedParameters };
+
+		//Act
+		var mutatedState = sut.Mutate(state, command);
+
+		//Assert
+		mutatedState.SelectedComponentParameters.ShouldBe(command.Parameters);
+	}
+}
