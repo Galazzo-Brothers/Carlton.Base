e1a47da6ae84d29cd77d6014a49a7be9425b8abe
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-20 11:09:00 -0400

    restructure

--- src/Infrastructure/InProcessMessaging/Carlton.Core.InProcessMessaging/Commands/ICommandHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Carlton.Core.InProcessMessaging.Commands;
-
-
-public interface ICommandHandler<in TCommand, TCommandResult>
-{
-    public Task<TCommandResult> Handle(TCommand command, CancellationToken cancellationToken);
-}
-
--- src/Infrastructure/InProcessMessaging/Carlton.Core.InProcessMessaging/Notifications/INotificationHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Carlton.Core.InProcessMessaging.Notifications;
-
-public interface INotificationHandler<TNotification>
-{
-    public Task Handle(TNotification notification, CancellationToken cancellationToken);
-}
