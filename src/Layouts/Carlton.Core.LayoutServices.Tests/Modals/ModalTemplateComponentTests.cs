e0b4327544c8b127add95561f3033609d2778f22
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-03-27 19:46:51 -0400

    more test cleanup

---
+++ src/Layouts/Carlton.Core.LayoutServices.Tests/Modals/ModalTemplateComponentTests.cs
@@ -0,0 +1,32 @@
+﻿using Carlton.Core.LayoutServices.Modals;
+namespace Carlton.Core.LayoutServices.Tests.Modals;
+
+[Trait("Component", nameof(ModalTemplate))]
+public class ModalTemplateComponentTests : TestContext
+{
+	private const string ModalContentTemplate = @"<span class=""modal-prompt"">{0}</span><span class=""modal-message"">{1}</span>";
+
+	[Theory(DisplayName = "Markup Test"), AutoData]
+	public void Modal_Markup_RendersCorrectly(
+	bool expectedIsVisible,
+		string expectedModalPrompt,
+		string expectedModalMessage)
+	{
+		//Arrange
+		var expectedMarkup =
+@$"<div class=""modal {(expectedIsVisible ? "visible" : string.Empty)}"">
+    <span class=""modal-prompt"">{expectedModalPrompt}</span>
+	<span class=""modal-message"">{expectedModalMessage}</span>
+</div>";
+
+		//Act
+		var cut = RenderComponent<ModalTemplate>(parameters => parameters
+		 .Add(p => p.IsVisible, expectedIsVisible)
+		 .Add(p => p.Prompt, expectedModalPrompt)
+		 .Add(p => p.Message, expectedModalMessage)
+		 .Add(p => p.ModalContent, state => string.Format(ModalContentTemplate, expectedModalPrompt, expectedModalMessage)));
+
+		//Assert
+		cut.MarkupMatches(expectedMarkup);
+	}
+}
