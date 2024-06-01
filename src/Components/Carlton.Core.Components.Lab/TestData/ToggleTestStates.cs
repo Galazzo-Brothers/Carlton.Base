834a846d628c76cf32f9e4d0a01c26f14988520d
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-06-01 16:21:40 -0400

    added toggle select to the components lab

---
+++ src/Components/Carlton.Core.Components.Lab/TestData/ToggleTestStates.cs
@@ -0,0 +1,13 @@
+﻿namespace Carlton.Core.Components.Lab.TestData;
+
+internal static class ToggleTestStates
+{
+	public static object DefaultState
+	{
+		get => new
+		{
+			FirstOption = new KeyValuePair<string, int>("Option 1", 1),
+			SecondOption = new KeyValuePair<string, int>("Option 2", 2)
+		};
+	}
+}
