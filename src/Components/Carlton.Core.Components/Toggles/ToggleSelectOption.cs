e3024696a86f17efd97c41d3a4ac1204e67eca00
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-06-01 20:19:33 -0400

    updated FluxStateViewer to use ViewState to persist toggle state

---
+++ src/Components/Carlton.Core.Components/Toggles/ToggleSelectOption.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+namespace Carlton.Core.Components.Toggles;
+
+/// <summary>
+/// Enumeration representing the options of the toggle select.
+/// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum ToggleSelectOption
+{
+	FirstOption = 1,
+	SecondOption = 2
+}
