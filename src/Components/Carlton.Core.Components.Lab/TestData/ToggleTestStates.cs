ea8a0719203ff1414f7e03a114b601b16d22d73a
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-06-23 16:06:23 -0400

    more toggle select states

--- src/Components/Carlton.Core.Components.Lab/TestData/ToggleTestStates.cs
+++ src/Components/Carlton.Core.Components.Lab/TestData/ToggleTestStates.cs
@@ -1,13 +1,26 @@
-﻿namespace Carlton.Core.Components.Lab.TestData;
+﻿using Carlton.Core.Components.Toggles;
+
+namespace Carlton.Core.Components.Lab.TestData;
 
 internal static class ToggleTestStates
 {
-	public static object DefaultState
+	public static object FirstOptionSelected
 	{
 		get => new
 		{
 			FirstOption = new KeyValuePair<string, int>("Option 1", 1),
-			SecondOption = new KeyValuePair<string, int>("Option 2", 2)
+			SecondOption = new KeyValuePair<string, int>("Option 2", 2),
+			SelectedOption = ToggleSelectOption.FirstOption
+		};
+	}
+
+	public static object SecondOptionSelected
+	{
+		get => new
+		{
+			FirstOption = new KeyValuePair<string, int>("Option 1", 1),
+			SecondOption = new KeyValuePair<string, int>("Option 2", 2),
+			SelectedOption = ToggleSelectOption.SecondOption
 		};
 	}
 }
