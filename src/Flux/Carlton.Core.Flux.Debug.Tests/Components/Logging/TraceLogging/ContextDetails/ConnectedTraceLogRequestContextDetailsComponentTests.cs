c9859546273b6984e98a1c3836e66d6649fc3365
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-06-26 22:48:53 -0400

    additional flux debug tests

---
+++ src/Flux/Carlton.Core.Flux.Debug.Tests/Components/Logging/TraceLogging/ContextDetails/ConnectedTraceLogRequestContextDetailsComponentTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture.Xunit2;
+using Bunit;
+using Bunit.TestDoubles;
+using Carlton.Core.Flux.Debug.Components;
+using Carlton.Core.Flux.Debug.Components.Logging.TraceLogging.ContextDetails;
+using Carlton.Core.Flux.Debug.Models.ViewModels;
+using Shouldly;
+using Xunit;
+namespace Carlton.Core.Flux.Debug.Tests.Components.Logging.TraceLogging.ContextDetails;
+
+public class ConnectedTraceLogRequestContextDetailsComponentTests : TestContext
+{
+	[Theory, AutoData]
+	public void ConnectedTraceLogRequestContextDetails_Markup_RendersCorrectly(TraceLogRequestContextDetailsViewModel expectedViewModel)
+	{
+		//Arrange
+		ComponentFactories.AddStub<TraceLogRequestContextDetails>("<div>TraceLogRequestContextDetails Stub</div>");
+		var expectedMarkup = @"<div>TraceLogRequestContextDetails Stub</div>";
+
+		//Act
+		var cut = RenderComponent<ConnectedTraceLogRequestContextDetails>(parameters => parameters
+			.Add(p => p.ViewModel, expectedViewModel));
+
+		//Assert
+		cut.MarkupMatches(expectedMarkup);
+	}
+
+	[Theory, AutoData]
+	public void TraceLogRequestContextDetails_ViewModelParameter_RendersCorrectly(TraceLogRequestContextDetailsViewModel expectedViewModel)
+	{
+		//Arrange
+		ComponentFactories.AddStub<TraceLogRequestContextDetails>("<div>JsonViewerConsole Stub</div>");
+
+		//Act
+		var cut = RenderComponent<ConnectedTraceLogRequestContextDetails>(parameters => parameters
+			.Add(p => p.ViewModel, expectedViewModel));
+		var stub = cut.FindComponent<Stub<TraceLogRequestContextDetails>>();
+
+		//Assert
+		stub.Instance.Parameters.Get(x => x.SelectedRequestContext).ShouldBe(expectedViewModel.SelectedRequestContext);
+	}
+}
