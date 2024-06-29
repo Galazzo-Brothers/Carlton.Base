770f27be2229353f7d4a714285b6181cab171176
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-06-29 11:52:35 -0400

    moved flux debug projects to a subfolder with its own readme

--- src/Flux/Carlton.Core.Flux.Debug.Models/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using System.ComponentModel.DataAnnotations;
-global using Carlton.Core.Utilities.Logging;
-global using Carlton.Core.Utilities.Validation;
-global using Carlton.Core.Flux.Debug.Models.Common;
--- src/Flux/Carlton.Core.Flux.Debug/Components/StateViewer/SubmitMutationConsole/SubmitMutationArgs.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Carlton.Core.Flux.Debug.Components.StateViewer.SubmitMutationConsole;
-
-/// <summary>
-/// Provides data for the mutation submission event.
-/// </summary>
-/// <param name="MutationCommand">The mutation command to be submitted.</param>
-public sealed record SubmitMutationArgs(object MutationCommand);
