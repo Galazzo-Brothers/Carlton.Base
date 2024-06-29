a285dcd699ca85f09f00da355a3ee652116be752
Nicholas Galazzo <nicholas.galazzo@gmail.com>
2024-06-28 22:22:14 -0400

    added ViewModelProjectionViewer component tests

--- src/Flux/Carlton.Core.Flux.Debug.Tests/Common/TestState.cs
+++ src/Flux/Carlton.Core.Flux.Debug.Tests/Common/TestState.cs
@@ -1,6 +1,8 @@
-﻿namespace Carlton.Core.Flux.Debug.Tests.Common;
+﻿using Carlton.Core.Flux.Contracts;
 
-internal class TestState
+namespace Carlton.Core.Flux.Debug.Tests.Common;
+
+public class TestState
 {
 }
 
@@ -9,3 +11,35 @@ internal record TestCommand1();
 internal record TestCommand2();
 
 internal record TestCommand3();
+
+internal record TestViewModel1();
+
+internal record TestViewModel2();
+
+internal record TestViewModel3();
+
+internal record TestStateViewModelProjectionMapper : IViewModelProjectionMapper<TestState>
+{
+	public TViewModel Map<TViewModel>(TestState state)
+	{
+		Type viewModelType = typeof(TViewModel);
+
+		if (viewModelType == typeof(TestViewModel1))
+		{
+			return (dynamic)new TestViewModel1();
+		}
+
+		if (viewModelType == typeof(TestViewModel2))
+		{
+			return (dynamic)new TestViewModel2();
+		}
+
+		if (viewModelType == typeof(TestViewModel3))
+		{
+			return (dynamic)new TestViewModel3();
+		}
+
+		throw new Exception();
+	}
+}
+
